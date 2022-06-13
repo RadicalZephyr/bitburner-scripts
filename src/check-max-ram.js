fd8828ab41500154368c190fe9651f691d20b74b
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-13 11:43:48 -0700

    Improve check-max-ram script

--- src/check-max-ram.js
+++ src/check-max-ram.js
@@ -1,7 +1,9 @@
-import { getHighestPurchasableRamLevel, reportServerComplementCost } from './lib.js';
+import { formatGigaBytes, getHighestPurchasableRamLevel, reportServerComplementCost } from './lib.js';
 
 /** @param {NS} ns */
 export async function main(ns) {
-  let ram = getHighestPurchasableRamLevel(ns, ns.args[0]);
+  const percentSpend = ns.args[0] ? ns.args[0] : 1.0;
+  const ram = getHighestPurchasableRamLevel(ns, percentSpend);
   reportServerComplementCost(ns, ram);
+  ns.tprintf("highest possible RAM purchase is %s", formatGigaBytes(ns.getPurchasedServerMaxRam()));
 }
