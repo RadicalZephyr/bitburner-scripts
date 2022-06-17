b7f8d87421d018c407c0f3ae93b4811194de8ee7
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-16 21:08:21 -0700

    Improve usage of assign-gang script

--- src/assign-gang.ts
+++ src/assign-gang.ts
@@ -3,13 +3,15 @@ import type { AutocompleteData, NS } from "netscript";
 const crimes = ["Unassigned", "Mug People", "Deal Drugs", "Strongarm Civilians", "Run a Con", "Armed Robbery", "Traffick Illegal Arms", "Threaten & Blackmail", "Human Trafficking", "Terrorism", "Vigilante Justice", "Train Combat", "Train Hacking", "Train Charisma", "Territory Warfare"];
 
 export function autocomplete(_data: AutocompleteData, _args: string[]) {
-    return crimes.map(crime => '"' + crime + '"');
+    return crimes;
 }
 
 export async function main(ns: NS) {
-    const task = ns.args[0];
+    const task = ns.args.join(" ");
     if (typeof task !== "string" || !crimes.includes(task)) {
         ns.tprintf("error: Unknown task specified '%s'", task);
+        const formattedTaskNames = crimes.map(crime => " - " + crime).join("\n");
+        ns.tprintf("Please choose one of the following tasks:\n%s", formattedTaskNames);
         return;
     }
 
