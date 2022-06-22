3fa3b6033330b79bb78ec9c2a46f69eb29fe4cd8
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-22 15:44:43 -0700

    Cleanup old temporary/experimental scripts

--- src/only-weaken.js
+++
@@ -1,3 +0,0 @@
-export async function main(ns) {
-  while (true) { await ns.weaken(ns.args[0]); }
-}
--- src/test.js
+++
@@ -1,9 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-  let player = ns.getPlayer();
-  let hackPercent = ns.formulas.hacking.hackPercent(ns.getServer("n00dles"), player);
-  let hackAnalyze = ns.hackAnalyze("n00dles");
-  ns.tprintf("hack percent of n00dles %s, hackAnalyze %s", hackPercent, hackAnalyze);
-  let sourceFiles = ns.getOwnedSourceFiles().map(sf => "n: " + sf.n + " lvl: " + sf.lvl);
-  ns.tprintf("owned source files %s []", sourceFiles.length, sourceFiles.join(","));
-}
