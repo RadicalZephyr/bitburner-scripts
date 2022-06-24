fd70610db99ddd19d045bf32204feea930a3935c
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-23 18:31:26 -0700

    [batch] Start sketching a thread packing algorithm

--- src/batch/types.ts
+++ src/batch/types.ts
@@ -1,6 +1,21 @@
 export type BatchSpec = {
+    host: string,
     script: string,
     threads: number,
     target: string
     delay: number,
+};
+
+export type HostSpec = {
+    host: string,
+    threads: number,
+    cores: number
+};
+
+export type TargetSpec = {
+    host: string,
+    hackThreads: number,
+    hackWeakenThreads: number,
+    growThreads: number,
+    growWeakenThreads: number,
 };
