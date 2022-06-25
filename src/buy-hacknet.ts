1c8ab3f37b0e32a9544e79c64c6725976b5c81ce
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-25 04:25:13 -0700

    [buy-hacknet] Implement a basic hacknet node buying script

--- src/buy-hacknet.ts
+++ src/buy-hacknet.ts
@@ -1,8 +1,37 @@
 import type { NS } from "netscript";
 
 export async function main(ns: NS) {
-    ns.hacknet.numNodes();
-    ns.hacknet.getLevelUpgradeCost(0, 1);
-    let max = ns.hacknet.maxNumNodes();
-    ns.tprintf("max hacknet nodes %s", max);
+    const budget = 0.25;
+    const maxNodes = ns.hacknet.maxNumNodes();
+
+    let ownedNodes = ns.hacknet.numNodes();
+    let nextNodeCost = ns.hacknet.getPurchaseNodeCost();
+
+    while (ownedNodes < maxNodes && nextNodeCost < ns.getServerMoneyAvailable('home') * budget) {
+        const nodeIndex = ns.hacknet.purchaseNode();
+        if (nodeIndex === -1) break;
+
+        ++ownedNodes;
+
+        const numLevels = 160;
+        const levelUpgradeCost = ns.hacknet.getLevelUpgradeCost(nodeIndex, numLevels);
+
+        if (levelUpgradeCost < ns.getServerMoneyAvailable('home')) {
+            ns.hacknet.upgradeLevel(nodeIndex, numLevels);
+        }
+
+        const ramGB = 5;
+        const ramUpgradeCost = ns.hacknet.getRamUpgradeCost(nodeIndex, ramGB);
+
+        if (ramUpgradeCost < ns.getServerMoneyAvailable('home')) {
+            ns.hacknet.upgradeRam(nodeIndex, ramGB);
+        }
+
+        const numCores = 4;
+        const coreUpgradeCost = ns.hacknet.getCoreUpgradeCost(nodeIndex, numCores);
+
+        if (coreUpgradeCost < ns.getServerMoneyAvailable('home')) {
+            ns.hacknet.upgradeCore(nodeIndex, numCores);
+        }
+    }
 }
