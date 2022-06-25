bada4354ce0f9905eb63a8d4a13768bc6436903f
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-24 21:50:24 -0700

    [gang] Add a monitoring script

---
+++ src/gang/monitor.ts
@@ -0,0 +1,44 @@
+import type { NS } from "netscript";
+
+export async function main(ns: NS) {
+    const flags = ns.flags([
+        ['refreshrate', 200],
+        ['help', false],
+    ]);
+
+    if (flags.help) {
+        ns.tprint("This script helps visualize what's going on with your gang.");
+        ns.tprint(`USAGE: run ${ns.getScriptName()}`);
+        ns.tprint("Example:");
+        ns.tprint(`> run ${ns.getScriptName()}`);
+        return;
+    }
+    ns.tail();
+    ns.disableLog('ALL');
+
+    const memberNames = ns.gang.getMemberNames();
+
+    while (true) {
+        const members = memberNames.map(name => ns.gang.getMemberInformation(name));
+
+        ns.clearLog();
+        ns.print('Member Hacking Info');
+        ns.printf("  %-9s | %9s %9s %9s %9s %9s %9s", 'member', 'exp', 'aexp', 'lvl', 'mult', 'amult', 'abonus');
+        ns.printf("--%'--9s-|-%'-9s-%'-9s-%'-9s-%'-9s-%'-9s-%'-9s", '', '', '', '', '', '', '');
+        for (const member of members) {
+            const ascResult = ns.gang.getAscensionResult(member.name);
+            ns.printf(
+                '  %-9s | %9s %9s %9s %9s %9s %9s',
+                member.name,
+                member.hack_exp.toFixed(0),
+                member.hack_asc_points.toFixed(0),
+                member.hack.toFixed(0),
+                member.hack_mult.toFixed(2),
+                member.hack_asc_mult.toFixed(2),
+                ascResult ? ascResult.hack.toFixed(6) : ''
+            );
+        }
+        ns.print('\n');
+        await ns.sleep(flags.refreshrate);
+    }
+}
