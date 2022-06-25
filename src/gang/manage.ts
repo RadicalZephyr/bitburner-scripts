2a1bdb9eaa41191228ca3bc4f740286120334e53
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-25 03:30:12 -0700

    [gang] Increase training percent

--- src/gang/manage.ts
+++ src/gang/manage.ts
@@ -1,7 +1,7 @@
 import type { GangMemberAscension, GangMemberInfo, NS } from "netscript";
 
 const ascendThreshold = 1.5;
-const trainingPercent = 0 / 12;
+const trainingPercent = 4 / 12;
 
 const maxPenalty = 0.05;
 const minWantedLevel = 10.0;
