887af96e99e2bfd6f1f1f69783914b5b59dc3e5e
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-26 08:37:47 -0700

    [batch] Spin up as many weaken threads as we can

--- src/batch/manage.ts
+++ src/batch/manage.ts
@@ -1,5 +1,11 @@
 import type { NS, Server } from "netscript";
 
+const scripts = {
+    'grow': { 'name': '/batch/grow.js', 'ram': 0 },
+    'hack': { 'name': '/batch/hack.js', 'ram': 0 },
+    'weaken': { 'name': '/batch/weaken.js', 'ram': 0 }
+};
+
 export async function main(ns: NS) {
     const hostsJSON = ns.args[0];
     if (typeof hostsJSON != 'string') {
@@ -8,14 +14,50 @@ export async function main(ns: NS) {
     }
     let servers: Server[] = JSON.parse(hostsJSON);
 
-    usabl
+    scripts.grow.ram = ns.getScriptRam(scripts.grow.name);
+    scripts.hack.ram = ns.getScriptRam(scripts.hack.name);
+    scripts.weaken.ram = ns.getScriptRam(scripts.weaken.name);
+
     let usableServers = servers.filter(s => canNuke(ns, s));
     usableServers.sort(byAvailableRam);
+
+    const totalAvailableRam = usableServers.map(s => s.maxRam - s.ramUsed).reduce((acc, x) => acc + x);
 
     let exploitableServers = servers.filter(s => hasMoney(ns, s));
     let currentlyExploitableServers = exploitableServers.filter(s => canHack(ns, s));
 
     currentlyExploitableServers.sort(bySoftest);
+
+    let softenSpecs = currentlyExploitableServers.map(s => analyzeSoftenTarget(ns, target));
+
+    let weakenProcesses = [];
+    for (const spec of softenSpecs) {
+        let remainingThreads = spec.threads;
+
+        while (remainingThreads > 0 && usableServers.length > 0) {
+            let server = usableServers[0];
+            let availableThreads = getAvailableThreads(ns, server, scripts.weaken.ram);
+
+            if (remainingThreads > availableThreads) {
+                const pid = ns.exec(scripts.weaken.name, server.ip, availableThreads, spec.host);
+                if (pid != 0) {
+                    remainingThreads -= availableThreads;
+                    usableServers.shift();
+                    weakenProcesses.push({ 'pid': pid, 'host': server.ip });
+                }
+            } else {
+                const pid = ns.exec(scripts.weaken.name, server.ip, remainingThreads, spec.host);
+                if (pid != 0) {
+                    remainingThreads = 0;
+                    weakenProcesses.push({ 'pid': pid, 'host': server.ip });
+                }
+            }
+        }
+
+        if (usableServers.length == 0) {
+            break;
+        }
+    }
 }
 
 function byAvailableRam(a: Server, b: Server): number {
@@ -23,7 +65,30 @@ function byAvailableRam(a: Server, b: Server): number {
 }
 
 function bySoftest(a: Server, b: Server): number {
-    return (a.hackDifficulty - a.baseDifficulty) - (b.hackDifficulty - b.baseDifficulty);
+    return (a.hackDifficulty - a.minDifficulty) - (b.hackDifficulty - b.minDifficulty);
+}
+
+function getAvailableThreads(ns: NS, server: Server, ram: number) {
+    const availableRam = server.maxRam - ns.getServerUsedRam(server.ip);
+    return Math.floor(availableRam / ram);
+}
+
+type WeakenTargetSpec = {
+    host: string,
+    ram: number,
+    threads: number,
+    time: number,
+};
+
+function analyzeSoftenTarget(ns: NS, target: Server): WeakenTargetSpec {
+    const threads = weakenAnalyze(ns, target, 1.0);
+    const time = ns.getWeakenTime(target.ip);
+    return {
+        'host': target,
+        'ram': threads * scripts.weaken.ram,
+        'threads': threads,
+        'time': time
+    };
 }
 
 /** Check if we can hack this server.
@@ -103,9 +168,9 @@ export function hackAnalyze(ns: NS, target: string, hackAmount: number): number 
 /** Calculate the number of threads needed to weaken the `target` by
  * the given multiplier.
  */
-export function weakenAnalyze(ns: NS, target: string, weakenAmount: number): number {
-    const currentSecurity = ns.getServerSecurityLevel(target);
-    const minSecurity = ns.getServerMinSecurityLevel(target);
+export function weakenAnalyze(ns: NS, target: Server, weakenAmount: number): number {
+    const currentSecurity = target.hackDifficulty;
+    const minSecurity = target.minDifficulty;
     return weakenThreads((currentSecurity - minSecurity) * weakenAmount);
 }
 
