446a3914adb91c5ab9d8f8d0d6d86f415d6047c3
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-26 23:15:18 -0700

    [gang] Format multiple stats more reasonably

--- src/gang/monitor.ts
+++ src/gang/monitor.ts
@@ -22,77 +22,46 @@ export async function main(ns: NS) {
         ns.clearLog();
 
         const memberNames = ns.gang.getMemberNames();
-        const maxNameLength = Math.max(...memberNames.map(n => n.length));
         const members = memberNames.map(name => ns.gang.getMemberInformation(name));
-        const maxTaskLength = Math.max(...members.map(m => m.task.length));
 
-        const nameTaskFormatPrefix = ` %-${maxNameLength}s | %${maxTaskLength}s `;
-        const baseFormatString = isHacking ?
-            nameTaskFormatPrefix + '%6s %7s %9s %9s' :
-            '\n' + nameTaskFormatPrefix +
-            '\n' + nameTaskFormatPrefix + '%6s %7s %9s %9s' +
-            '\n' + nameTaskFormatPrefix + '%6s %7s %9s %9s' +
-            '\n' + nameTaskFormatPrefix + '%6s %7s %9s %9s'
-            ;
+        const nameTaskFormat = ' %s\n task: %s';
+        const baseFormatString = ' %4s | %5s %5s %9s %7s %10s';
         const dividerFormatString = baseFormatString.replaceAll(' ', '-').replaceAll('%', "%'-");
 
         const gangType = isHacking ? 'Hacking' : 'Combat';
         ns.print(`Member ${gangType} Info\n`);
 
-        const headings = isHacking ?
-            ['hack', 'hack_xp', 'hack_mul', 'hack_asc'] :
-            [
-                '', '', 'str', 'def', 'dex', 'agi',
-                '', '', 'str_xp', 'def_xp', 'dex_xp', 'agi_xp',
-                '', '', 'str_mul', 'def_mul', 'dex_mul', 'agi_mul',
-                '', '', 'str_asc', 'def_asc', 'dex_asc', 'agi_asc'
-            ];
+        const headings = ['stat', 'lvl', 'mul', 'asc_bonus', 'xp', 'asc_points'];
         const blanks = Array(headings.length).fill('');
 
         ns.printf(baseFormatString, 'member', 'task', ...headings);
-        ns.printf(dividerFormatString, ...blanks);
         for (const member of members) {
+            ns.printf(nameTaskFormat, member.name, member.task);
+            ns.printf(baseFormatString, ...headings);
+            ns.printf(dividerFormatString, ...blanks);
+
             const ascResult = ns.gang.getAscensionResult(member.name);
-            const stats = isHacking ?
-                [
-                    member.hack.toFixed(0),
-                    member.hack_exp.toFixed(0),
-                    member.hack_asc_mult.toFixed(2),
-                    ascResult ? ascResult.hack.toFixed(6) : ''
-                ] :
-                [
-                    '', '',
-                    member.str.toFixed(0),
-                    member.str_exp.toFixed(0),
-                    member.str_asc_mult.toFixed(2),
-                    ascResult ? ascResult.str.toFixed(6) : '',
 
-                    '', '',
-                    member.def.toFixed(0),
-                    member.def_exp.toFixed(0),
-                    member.def_asc_mult.toFixed(2),
-                    ascResult ? ascResult.def.toFixed(6) : '',
-
-                    '', '',
-                    member.dex.toFixed(0),
-                    member.dex_exp.toFixed(0),
-                    member.dex_asc_mult.toFixed(2),
-                    ascResult ? ascResult.dex.toFixed(6) : '',
-
-                    '', '',
-                    member.agi.toFixed(0),
-                    member.agi_exp.toFixed(0),
-                    member.agi_asc_mult.toFixed(2),
-                    ascResult ? ascResult.agi.toFixed(6) : ''
+            const stats = isHacking ? ['hack', 'cha'] : ['str', 'def', 'dex', 'agi'];
+            for (const stat of stats) {
+                const memberAny: any = member;
+                const ascResultAny: any = ascResult;
+                const memberStatDetails = [
+                    stat,
+                    memberAny[stat].toFixed(0),
+                    memberAny[stat + '_asc_mult'].toFixed(2),
+                    ascResult ? ascResultAny[stat].toFixed(6) : '',
+                    memberAny[stat + '_exp'].toFixed(0),
+                    memberAny[stat + '_asc_points'].toFixed(0)
                 ];
-            ns.printf(
-                baseFormatString,
-                member.name,
-                member.task,
-                ...stats
-            );
+
+                ns.printf(
+                    baseFormatString,
+                    ...memberStatDetails
+                );
+            }
+            ns.print('');
         }
-        ns.print('');
         await ns.sleep(flags.refreshrate);
     }
 }
