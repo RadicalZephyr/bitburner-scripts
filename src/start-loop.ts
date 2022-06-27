5aba46e97f78d2c31020a2dfda3a2b56d4ba1ee9
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-26 20:14:06 -0700

    [start-loop] Add a loop hacking start script

---
+++ src/start-loop.ts
@@ -0,0 +1,16 @@
+import type { NS } from "netscript";
+
+import { walkNetworkBFS } from "./walk-network.js";
+
+const scriptList = ['/loop/grow.js', '/loop/hack.js', '/loop/weaken.js'];
+
+export async function main(ns: NS) {
+    let network = walkNetworkBFS(ns);
+    let hostNames: string[] = Array.from(network.keys()).filter(h => h !== 'home');
+
+    // Deploy all loop scripts to all servers
+    for (const host of hostNames) {
+        await ns.scp(scriptList, host);
+    }
+
+}
