0df694d9cbefa3e0ec6ac2a7c2cc931bc2f9d89c
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-30 18:15:53 -0700

    [solve-contracts] De-nest solver loop

--- src/solve-contracts.ts
+++ src/solve-contracts.ts
@@ -32,54 +32,57 @@ export async function main(ns: NS) {
 
     for (const contract of contracts) {
         const programName = '/contracts/' + contract.contract_name.replaceAll(' ', '-') + '.js';
-        if (ns.fileExists(programName)) {
-            const contractData: number = ns.codingcontract.getData(contract.file, contract.host);
-            const stringContractData = JSON.stringify(contractData);
 
-            const pid = ns.run(programName, 1, stringContractData)
-            if (pid !== 0) {
-                // Sleep until the solver script exits
-                while (ns.getRunningScript(pid)) await ns.sleep(200);
-
-                const contractPort = ns.getPortHandle(CONTRACT_PORT);
-                // Get output from contract solving program
-                let answer = contractPort.read();
-
-                // If the port data isn't a valid answer then just skip this contract
-                if (typeof answer == 'string' && answer === 'NULL PORT DATA') {
-                    ns.tprint(`
-No answer received for contract ${} from ${programName}.
-Program should write the answer to the contract to port ${CONTRACT_PORT}.
-`);
-                    continue;
-                }
-                let contractAnswer: number | string[];
-                if (typeof answer == 'string') {
-                    contractAnswer = [answer];
-                } else {
-                    contractAnswer = answer;
-                }
-
-                const options = { 'returnReward': true };
-                // Attempt to submit it to the contract
-                const contractReward = ns.codingcontract.attempt(contractAnswer, contract.file, contract.host, options);
-
-                if (typeof contractReward == 'string') {
-                    if (contractReward === '') {
-                        ns.tprint(`failed contract`);
-                    } else {
-                        ns.tprint(`result of solving contract: ${contractReward}`);
-                    }
-                }
-            } else {
-                ns.tprint(`Could not run ${programName}, try running manually:
-run ${programName} "${stringContractData}"
-`);
-            }
-        } else {
+        if (!ns.fileExists(programName)) {
             ns.tprint(`Found unsolvable contract of type '${contract.contract_name}' in ${contract.file} on ${contract.host}
 Create a script ${programName} to solve this contract type!\n
 `);
+            continue;
+        }
+
+        const contractData: number = ns.codingcontract.getData(contract.file, contract.host);
+        const stringContractData = JSON.stringify(contractData);
+
+        const pid = ns.run(programName, 1, stringContractData)
+        if (pid === 0) {
+            ns.tprint(`Could not run ${programName}, try running manually:
+run ${programName} "${stringContractData}"
+`);
+            continue;
+        }
+
+        // Sleep until the solver script exits
+        while (ns.getRunningScript(pid)) await ns.sleep(200);
+
+        const contractPort = ns.getPortHandle(CONTRACT_PORT);
+        // Get output from contract solving program
+        let answer = contractPort.read();
+        // If the port data isn't a valid answer then just skip this contract
+        if (typeof answer == 'string' && answer === 'NULL PORT DATA') {
+            ns.tprint(`
+No answer received for contract ${} from ${programName}.
+Program should write the answer to the contract to port ${CONTRACT_PORT}.
+`);
+            continue;
+        }
+
+        let contractAnswer: number | string[];
+        if (typeof answer == 'string') {
+            contractAnswer = [answer];
+        } else {
+            contractAnswer = answer;
+        }
+
+        const options = { 'returnReward': true };
+        // Attempt to submit it to the contract
+        const contractReward = ns.codingcontract.attempt(contractAnswer, contract.file, contract.host, options);
+
+        if (typeof contractReward == 'string') {
+            if (contractReward === '') {
+                ns.tprint(`failed contract`);
+            } else {
+                ns.tprint(`result of solving contract: ${contractReward}`);
+            }
         }
     }
 }
