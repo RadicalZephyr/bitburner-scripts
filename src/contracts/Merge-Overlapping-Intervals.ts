cd676946a035b4c9f2e3799af610b4030478be39
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-30 17:15:39 -0700

    [contracts] Add starts for two more contract types

---
+++ src/contracts/Merge-Overlapping-Intervals.ts
@@ -0,0 +1,20 @@
+import type { NS } from "netscript";
+
+/* Given the following array of array of numbers representing a list
+ * of intervals, merge all overlapping intervals.
+ *
+ * [[8,17],[16,20],[19,29]]
+ *
+ * Example:
+ *
+ * [[1, 3], [8, 10], [2, 6], [10, 16]]
+ *
+ * would merge into [[1, 6], [8, 16]].
+ *
+ * The intervals must be returned in ASCENDING order. You can assume
+ * that in an interval, the first number will always be smaller than
+ * the second.
+ */
+export async function main(ns: NS) {
+
+}
---
+++ src/contracts/Minimum-Path-Sum-in-a-Triangle.ts
@@ -0,0 +1,33 @@
+import type { NS } from "netscript";
+
+/* Given a triangle, find the minimum path sum from top to bottom. In
+ * each step of the path, you may only move to adjacent numbers in the
+ * row below. The triangle is represented as a 2D array of numbers:
+ *
+ * [
+ *            [6],
+ *           [9,9],
+ *          [6,2,7],
+ *         [2,8,3,3],
+ *        [2,1,3,9,8],
+ *       [5,9,2,7,1,8],
+ *      [4,7,6,2,4,8,6],
+ *     [1,4,4,1,4,2,6,1],
+ *    [4,5,9,2,7,3,4,9,3],
+ *   [9,7,7,9,1,7,4,3,5,6]
+ * ]
+ *
+ * Example: If you are given the following triangle:
+ *
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ *
+ * The minimum path sum is 11 (2 -> 3 -> 5 -> 1).
+*/
+export async function main(ns: NS) {
+
+}
