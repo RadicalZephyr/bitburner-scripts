dbf81dfc6353d4118149d5f14bbaad295a412d12
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-04 22:24:07 -0700

    [walk-network] Unify network walking functions to one impl with an option

--- src/walk-network.ts
+++ src/walk-network.ts
@@ -6,52 +6,48 @@ export async function main(ns: NS) {
     await ns.write("networkJSON.txt", JSON.stringify(Array.from(network.entries()), null, 2), "w");
 }
 
-/** Walk the network and return an array of all hosts.
- *
- */
 export function walkNetworkBFS(ns: NS): Map<string, string[]> {
-    let root = "home";
-    let q = [];
-    let explored = new Set();
-    let network = new Map();
-
-    explored.add(root);
-    q.push(root);
-
-    while (q.length > 0) {
-        let v = q.shift();
-
-        let edges = ns.scan(v);
-        network.set(v, edges);
-
-        for (const w of edges) {
-            if (!explored.has(w)) {
-                explored.add(w);
-                q.push(w);
-            }
-        }
-    }
-    return network;
+    return walkNetwork(ns, { 'order': 'breadth' });
 }
 
 export function walkNetworkDFS(ns: NS): Map<string, string[]> {
-    let root = "home";
-    let s = [];
+    return walkNetwork(ns, { 'order': 'depth' });
+}
+
+export type WalkOrder = 'breadth' | 'depth';
+
+export type WalkOptions = {
+    'order': WalkOrder,
+};
+
+type NextNodeFn = ((nodes: string[]) => string);
+
+/** Walk the network and return an array of all hosts.
+ *
+ */
+export function walkNetwork(ns: NS, options?: WalkOptions): Map<string, string[]> {
+    const nextNode: NextNodeFn = options && options.order === 'depth'
+        ? (n) => n.pop()
+        : (n) => n.shift();
+
+    let root = 'home';
+    let nodesToExplore = [];
     let explored = new Set();
     let network = new Map();
 
     explored.add(root);
-    s.push(root);
+    nodesToExplore.push(root);
 
-    while (s.length > 0) {
-        let v = s.pop();
+    while (nodesToExplore.length > 0) {
+        let v = nextNode(nodesToExplore);
 
         let edges = ns.scan(v);
         network.set(v, edges);
+
         for (const w of edges) {
             if (!explored.has(w)) {
                 explored.add(w);
-                s.push(w);
+                nodesToExplore.push(w);
             }
         }
     }
