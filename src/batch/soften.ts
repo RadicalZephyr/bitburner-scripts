0a9e7a38e0a611f424899566fadbcc57d3d00888
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-04 23:47:36 -0700

    [batch] Implement simple soften script

--- src/batch/soften.ts
+++ src/batch/soften.ts
@@ -1,6 +1,8 @@
 import type { NS } from "netscript";
 
-import { weakenAnalyze } from '../lib';
+import { numThreads, weakenAnalyze } from '../lib';
+
+const weakenScript = '/batch/weaken.js';
 
 export async function main(ns: NS) {
     const hostsJSON = ns.args[0];
@@ -17,20 +19,90 @@ export async function main(ns: NS) {
     }
     const targets: string[] = JSON.parse(targetsJSON);
     // Calculate initial weaken details for all targets (time, threads)
-    const targetSpecs = targets.map(t => analyzeSoftenTarget(ns, t));
+    let targetSpecs = targets.map(t => analyzeSoftenTarget(ns, t));
+
+    // Order targets by increasing weaken time
+    targetSpecs.sort(byTime);
+
+    ns.print('target weaken times:');
+    targetSpecs.forEach(t => ns.print(`${t.host}: ${t.time}`));
+
+    ns.print(`hosts: ${typeof hosts} = ${JSON.stringify(hosts)}`);
+    ns.print(`targets: ${typeof targets} = ${JSON.stringify(targets)}`);
 
     // N.B. no rounds are used in this stage because it's faster to
     // just weaken it in one go, so we start all the threads at once.
+    const waitTime = 0;
 
-    // Determine total number of threads available across all hosts
+    let weakenInstances: WeakenInstance[] = [];
+    let targetIndex = 0;
 
-    // Order targets by increasing weaken time
+    ns.print(`available threads`);
+    for (const host of hosts) {
+        let weakenThreads = numThreads(ns, host, weakenScript);
+        ns.print(`${host}: ${weakenThreads}`);
 
-    // Build a set of BatchSpecs for all hosts by greedily assigning threads
-    // to initially weaken targets
+        if (weakenThreads === 0) continue;
+        if (targetIndex >= targetSpecs.length) break;
 
-    // Include a batch spec for starting the building phase
+        let nextTarget = targetSpecs[targetIndex];
+        while (targetIndex < targetSpecs.length && weakenThreads > nextTarget.threads && nextTarget.threads > 0) {
+
+            const threadsToRun = nextTarget.threads;
+
+            const pid = ns.exec(weakenScript, host, threadsToRun, nextTarget.host, waitTime);
+
+            if (pid !== 0) {
+                ns.tprint(`spawned ${weakenScript} with ${threadsToRun} threads on ${host}`);
+                weakenInstances.push({
+                    'host': host,
+                    'pid': pid,
+                    'threads': threadsToRun
+                });
+                // Deduct just spawned threads from current host's available count
+                weakenThreads -= threadsToRun;
+            } else {
+                ns.tprint(`failed to spawn ${weakenScript} with ${threadsToRun} threads on ${host}`);
+            }
+
+            targetIndex += 1;
+            nextTarget = targetSpecs[targetIndex];
+        }
+
+        const threadsToRun = weakenThreads;
+
+        const pid = ns.exec(weakenScript, host, threadsToRun, nextTarget.host, waitTime);
+
+        if (pid !== 0) {
+            ns.tprint(`spawned ${weakenScript} with ${threadsToRun} threads on ${host}`);
+            weakenInstances.push({
+                'host': host,
+                'pid': pid,
+                'threads': threadsToRun
+            });
+            // Deduct just spawned threads from current target's desired threads
+            nextTarget.threads -= threadsToRun;
+
+            if (nextTarget.threads === 0) targetIndex += 1;
+        } else {
+            ns.tprint(`failed to spawn ${weakenScript} with ${threadsToRun} threads on ${host}`);
+        }
+    }
+
+    if (targetIndex >= targetSpecs.length) {
+        ns.tprint('launched all weaken threads for softening phase');
+    } else {
+        const remainingTargets = targetSpecs.slice(targetIndex);
+        const totalThreads = remainingTargets.reduce((acc, t) => acc + t.threads, 0);
+        ns.tprint(`ran out of hosts to run weaken on, still need to launch ${totalThreads} threads targeting ${remainingTargets.length} hosts`);
+    }
 }
+
+type WeakenInstance = {
+    host: string,
+    pid: number,
+    threads: number,
+};
 
 type TargetSpec = {
     host: string,
@@ -47,3 +119,5 @@ function analyzeSoftenTarget(ns: NS, target: string): TargetSpec {
         'time': time
     };
 }
+
+const byTime = (a: TargetSpec, b: TargetSpec) => a.time - b.time;
