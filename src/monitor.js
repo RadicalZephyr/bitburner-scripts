3e9ff1d2f2674065283b17f0fc4685599e903168
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-17 17:02:02 -0700

    [monitor] Restructure for fewer print statements

--- src/monitor.js
+++ src/monitor.js
@@ -12,20 +12,23 @@ export async function main(ns) {
   }
   ns.tail();
   ns.disableLog('ALL');
+
+  const server = flags._[0];
+  const maxMoney = ns.getServerMaxMoney(server);
+  const minSec = ns.getServerMinSecurityLevel(server);
+
   while (true) {
-    const server = flags._[0];
     let money = ns.getServerMoneyAvailable(server);
     if (money === 0) money = 1;
-    const maxMoney = ns.getServerMaxMoney(server);
-    const minSec = ns.getServerMinSecurityLevel(server);
     const sec = ns.getServerSecurityLevel(server);
     ns.clearLog(server);
-    ns.print(`${server}:`);
-    ns.print(` $_______: ${ns.nFormat(money, "$0.000a")} / ${ns.nFormat(maxMoney, "$0.000a")} (${(money / maxMoney * 100).toFixed(2)}%)`);
-    ns.print(` security: +${(sec - minSec).toFixed(2)}`);
-    ns.print(` hack____: ${ns.tFormat(ns.getHackTime(server))} (t=${Math.ceil(ns.hackAnalyzeThreads(server, money))})`);
-    ns.print(` grow____: ${ns.tFormat(ns.getGrowTime(server))} (t=${Math.ceil(ns.growthAnalyze(server, maxMoney / money))})`);
-    ns.print(` weaken__: ${ns.tFormat(ns.getWeakenTime(server))} (t=${Math.ceil((sec - minSec) * 20)})`);
+    ns.print(`${server}:
+ $_______: ${ns.nFormat(money, "$0.000a")} / ${ns.nFormat(maxMoney, "$0.000a")} (${(money / maxMoney * 100).toFixed(2)}%)
+ security: +${(sec - minSec).toFixed(2)}
+ hack____: ${ns.tFormat(ns.getHackTime(server))} (t=${Math.ceil(ns.hackAnalyzeThreads(server, money))})
+ grow____: ${ns.tFormat(ns.getGrowTime(server))} (t=${Math.ceil(ns.growthAnalyze(server, maxMoney / money))})
+ weaken__: ${ns.tFormat(ns.getWeakenTime(server))} (t=${Math.ceil((sec - minSec) * 20)})
+`);
     await ns.sleep(flags.refreshrate);
   }
 }
