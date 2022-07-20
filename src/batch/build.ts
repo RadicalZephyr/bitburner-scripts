ecf5b1b816c24f107499f63a2efb9afb4aa61f76
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-20 01:05:53 -0700

    [batch] Calculate delay times for script end timing

--- src/batch/build.ts
+++ src/batch/build.ts
@@ -2,6 +2,8 @@ import type { NS, AutocompleteData } from "netscript";
 
 import { growthAnalyze, numThreads, singleTargetBatchOptions, weakenThreads } from '../lib.js';
 
+const minimumTimeDelta = 20;
+
 const weakenScript = '/batch/weaken.js';
 const growScript = '/batch/grow.js';
 
@@ -12,24 +14,31 @@ export function autocomplete(data: AutocompleteData, args: string[]): string[] {
 export async function main(ns: NS) {
     const [host, target] = singleTargetBatchOptions(ns);
 
-    const delay = 0;
-
     ns.kill('monitor.js', target);
     ns.run('monitor.js', 1, target);
 
     let maxHostThreads = numThreads(ns, host, growScript);
     const targetSpec = analyzeBuildTarget(ns, target);
 
+    const growTime = targetSpec.growTime;
     const growThreads = targetSpec.initialGrowthThreads;
+    // Calculate weaken time shifted to not include the minimum time
+    // delta it needs to end after grow does.
+    const weakenTime = targetSpec.weakenTime - minimumTimeDelta;
     const weakenThreads = targetSpec.postGrowthWeakenThreads;
     const totalThreads = growThreads + weakenThreads
+
     if (maxHostThreads > totalThreads && totalThreads > 0) {
         ns.tprint(`building ${target} with ${growThreads} grow threads and ${weakenThreads} weaken threads on ${host}`);
+
+        const growDelay = weakenTime > growTime ? weakenTime - growTime : 0;
+        const weakenDelay = growTime > weakenTime ? growTime - weakenTime : 0;
+
         if (growThreads > 0) {
-            ns.exec(weakenScript, host, weakenThreads, target, delay);
+            ns.exec(growScript, host, growThreads, target, growDelay);
         }
         if (weakenThreads > 0) {
-            ns.exec(growScript, host, growThreads, target, delay);
+            ns.exec(weakenScript, host, weakenThreads, target, weakenDelay);
         }
     } else {
         ns.tprint(`
@@ -49,7 +58,9 @@ trying to run ${growThreads} grow threads and ${weakenThreads} weaken threads
 
 export type GrowSpec = {
     host: string,
+    growTime: number,
     initialGrowthThreads: number,
+    weakenTime: number,
     postGrowthWeakenThreads: number,
 };
 
@@ -62,9 +73,14 @@ export function analyzeBuildTarget(ns: NS, target: string): GrowSpec {
 
     const postGrowthWeakenThreads = weakenThreads(initialGrowthSecurity);
 
+    const growTime = ns.getGrowTime(target);
+    const weakenTime = ns.getWeakenTime(target);
+
     return {
         'host': target,
+        'growTime': growTime,
         'initialGrowthThreads': initialGrowthThreads,
+        'weakenTime': weakenTime,
         'postGrowthWeakenThreads': postGrowthWeakenThreads,
     };
 }
