3c669057f5ab65dd3849f02e1b2a53347df74f52
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-20 03:30:11 -0700

    [batch] Implement basic single host/single target milk script

--- src/batch/milk.ts
+++ src/batch/milk.ts
@@ -1,28 +1,97 @@
-import type { NS } from "netscript";
+import type { NS, AutocompleteData } from "netscript";
 
-import { growthAnalyze, weakenThreads } from '../lib.js';
+import { growthAnalyze, numThreads, singleTargetBatchOptions, weakenThreads } from '../lib.js';
+
+const minimumTimeDelta = 20;
+
+const weakenScript = '/batch/weaken.js';
+const growScript = '/batch/grow.js';
+const hackScript = '/batch/hack.js';
+
+export function autocomplete(data: AutocompleteData, args: string[]): string[] {
+    return data.servers;
+}
 
 export async function main(ns: NS) {
-    const hostsJSON = ns.args[0];
-    if (typeof hostsJSON != 'string') {
-        ns.printf('invalid hosts list');
-        return;
-    }
-    const hosts: string[] = JSON.parse(hostsJSON);
+    const [host, target] = singleTargetBatchOptions(ns);
 
-    const targetsJSON = ns.args[1];
-    if (typeof targetsJSON != 'string') {
-        ns.printf('invalid targets list');
-        return;
-    }
-    const targets: string[] = JSON.parse(targetsJSON);
+    ns.kill('monitor.js', target);
+    ns.run('monitor.js', 1, target);
 
-    // Calculate growth, weaken and hack details for all targets
-    const targetSpecs = targets.map(t => analyzeMilkTarget(ns, t));
+    let maxHostThreads = numThreads(ns, host, growScript);
+    const targetSpec = analyzeMilkTarget(ns, target);
+
+    const growTime = targetSpec.growTime;
+    const hackTime = targetSpec.hackTime;
+    const weakenTime = targetSpec.weakenTime;
+
+    const hackThreads = targetSpec.hackThreads;
+    const postHackWeakenThreads = targetSpec.postHackWeakenThreads;
+    const growThreads = targetSpec.growthThreads;
+    const postGrowthWeakenThreads = targetSpec.postGrowthWeakenThreads;
+
+    const totalThreads = hackThreads + postHackWeakenThreads + growThreads + postGrowthWeakenThreads;
+
+    if (maxHostThreads > totalThreads && totalThreads > 0) {
+        ns.tprint(`
+milking ${target} from ${host}:
+  ${hackThreads} hack threads
+  ${postHackWeakenThreads} post-hack weaken threads
+  ${growThreads} grow threads
+  ${postGrowthWeakenThreads} post-grow weaken threads
+`);
+
+        // Calculate script ending times relative to longest running time
+        // FIXME: Assume for now that weakenTime > growTime > hackTime
+
+        // Post-hack weaken dictates relative timing of the other scripts
+        const postHackWeakenEndTime = weakenTime;
+        // Hack should finish delta T before it's weaken.
+        const hackEndTime = postHackWeakenEndTime - minimumTimeDelta;
+        // Grow should finish delta T after first weaken
+        const growEndTime = postHackWeakenEndTime + minimumTimeDelta;
+        // Post-grow weaken should finish delta T after it
+        const postGrowWeakenEndTime = growEndTime + minimumTimeDelta;
+
+        // Calculate start times using end times and script durations
+
+        // N.B. this should be 0
+        const postHackWeakenStartTime = postHackWeakenEndTime - weakenTime;
+        // N.B. this should be 2*minimumTimeDelta
+        const postGrowWeakenStartTime = postGrowWeakenEndTime - weakenTime;
+
+        const growStartTime = growEndTime - growTime;
+        const hackStartTime = hackEndTime - hackTime;
+
+        if (postHackWeakenThreads > 0) {
+            ns.exec(weakenScript, host, postHackWeakenThreads, target, postHackWeakenStartTime);
+        }
+        if (postGrowthWeakenThreads > 0) {
+            ns.exec(weakenScript, host, postGrowthWeakenThreads, target, postGrowWeakenStartTime);
+        }
+        if (growThreads > 0) {
+            ns.exec(growScript, host, growThreads, target, growStartTime);
+        }
+        if (hackThreads > 0) {
+            ns.exec(hackScript, host, hackThreads, target, hackStartTime);
+        }
+    } else {
+        ns.tprint(`
+not enough threads to run milk on ${host}
+trying to run:
+  ${hackThreads} hack threads
+  ${postHackWeakenThreads} post-hack weaken threads
+  ${growThreads} grow threads
+  ${postGrowthWeakenThreads} post-grow weaken threads
+`);
+    }
 }
 
 export type HackSpec = {
     host: string,
+    growTime: number,
+    hackTime: number,
+    weakenTime: number,
     hackThreads: number,
     postHackWeakenThreads: number,
     growthThreads: number,
@@ -39,11 +108,17 @@ export function analyzeMilkTarget(ns: NS, target: string): HackSpec {
 
     const hackShrinkage = ns.hackAnalyze(target) * hackThreads;
     const growthThreads = growthAnalyze(ns, target, 1 / (1 - hackShrinkage));
-    const growthSecurityIncrease = ns.growthAnalyzeSecurity(growthThreads, target);
+    const growthSecurityIncrease = ns.growthAnalyzeSecurity(growthThreads, target, 1);
     const postGrowthWeakenThreads = weakenThreads(growthSecurityIncrease);
 
+    const growTime = ns.getGrowTime(target);
+    const weakenTime = ns.getWeakenTime(target);
+    const hackTime = ns.getHackTime(target);
     return {
         'host': target,
+        'growTime': growTime,
+        'hackTime': hackTime,
+        'weakenTime': weakenTime,
         'hackThreads': hackThreads,
         'postHackWeakenThreads': postHackWeakenThreads,
         'growthThreads': growthThreads,
