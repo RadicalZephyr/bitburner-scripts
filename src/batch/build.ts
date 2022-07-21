cc186e1ed3af0f55ef689752cadece50e29df2ea
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-21 02:17:13 -0700

    [batch] Inline build analysis fn

--- src/batch/build.ts
+++ src/batch/build.ts
@@ -1,6 +1,6 @@
 import type { NS, AutocompleteData } from "netscript";
 
-import { growAnalyze, numThreads, singleTargetBatchOptions, weakenThreads } from '../lib.js';
+import { growAnalyze, numThreads, singleTargetBatchOptions, weakenThreads as weakenThreadsFn } from '../lib.js';
 
 const minimumTimeDelta = 20;
 
@@ -18,13 +18,19 @@ export async function main(ns: NS) {
     ns.run('monitor.js', 1, target);
 
     let maxHostThreads = numThreads(ns, host, growScript);
-    const targetSpec = analyzeBuildTarget(ns, target);
 
-    const growTime = targetSpec.growTime;
-    const growThreads = targetSpec.initialGrowThreads;
+    const maxMoney = ns.getServerMaxMoney(target);
+    const currentMoney = ns.getServerMoneyAvailable(target);
 
-    const weakenTime = targetSpec.weakenTime;
-    const weakenThreads = targetSpec.postGrowWeakenThreads;
+    const neededGrowRatio = currentMoney > 0 ? maxMoney / currentMoney : maxMoney;
+
+    const growTime = ns.getGrowTime(target);
+    const growThreads = growAnalyze(ns, target, neededGrowRatio);
+
+    const growSecurityIncrease = ns.growthAnalyzeSecurity(growThreads, target, 1);
+
+    const weakenTime = ns.getWeakenTime(target);
+    const weakenThreads = weakenThreadsFn(growSecurityIncrease);
 
     const totalThreads = growThreads + weakenThreads
 
@@ -58,35 +64,4 @@ trying to run ${growThreads} grow threads and ${weakenThreads} weaken threads
             ns.tprint(`${target} does not need to be built`);
         }
     }
-}
-
-export type GrowSpec = {
-    host: string,
-    growTime: number,
-    initialGrowThreads: number,
-    weakenTime: number,
-    postGrowWeakenThreads: number,
-};
-
-export function analyzeBuildTarget(ns: NS, target: string): GrowSpec {
-    const maxMoney = ns.getServerMaxMoney(target);
-    const currentMoney = ns.getServerMoneyAvailable(target);
-
-    const neededGrowRatio = currentMoney > 0 ? maxMoney / currentMoney : maxMoney;
-
-    const growTime = ns.getGrowTime(target);
-    const growThreads = growAnalyze(ns, target, neededGrowRatio);
-
-    const growSecurityIncrease = ns.growthAnalyzeSecurity(growThreads, target, 1);
-
-    const weakenTime = ns.getWeakenTime(target);
-    const postGrowWeakenThreads = weakenThreads(growSecurityIncrease);
-
-    return {
-        'host': target,
-        'growTime': growTime,
-        'initialGrowThreads': growThreads,
-        'weakenTime': weakenTime,
-        'postGrowWeakenThreads': postGrowWeakenThreads,
-    };
 }
