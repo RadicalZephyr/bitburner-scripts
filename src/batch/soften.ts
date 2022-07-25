5e53d1e7793b1ed3286ceab6f053ab5f3926970b
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-24 22:28:06 -0700

    [batch] Extract function to calculate weakenInstance

--- src/batch/soften.ts
+++ src/batch/soften.ts
@@ -13,21 +13,25 @@ export async function main(ns: NS) {
 
     let maxHostThreads = numThreads(ns, host, weakenScript);
 
-    let script = weakenScript;
-    let threads = weakenAnalyze(ns, target, 1.0);
-    const runTime = ns.getWeakenTime(target);
-    const weakenInstance = { script, threads, host, target, startTime: 0, runTime };
+    var weakenInstance = calculateWeakenInstance(ns, host, target);
 
-    if (maxHostThreads > 0 && maxHostThreads > threads) {
-        ns.tprint(`softening ${target} with ${threads} threads on ${host}`);
+    if (maxHostThreads > 0 && maxHostThreads > weakenInstance.threads) {
+        ns.tprint(`softening ${target} with ${weakenInstance.threads} threads on ${host}`);
         spawnBatchScript(ns, weakenInstance);
     } else {
         if (maxHostThreads < 1) {
             ns.tprint(`not enough RAM available to run weaken on ${host}`);
         }
 
-        if (threads < 1) {
+        if (weakenInstance.threads < 1) {
             ns.tprint(`${target} does not need to be weakened`);
         }
     }
+}
+
+function calculateWeakenInstance(ns: NS, host: string, target: string) {
+    let script = weakenScript;
+    let threads = weakenAnalyze(ns, target, 1.0);
+    const runTime = ns.getWeakenTime(target);
+    return { script, threads, host, target, startTime: 0, runTime };
 }
