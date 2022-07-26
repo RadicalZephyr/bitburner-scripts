7d0b3f89534840e0594f3028de1900fe193ce8c7
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-26 00:57:04 -0700

    [batch] Recalculate available threads after each spawning each milk batch

--- src/batch/milk.ts
+++ src/batch/milk.ts
@@ -47,8 +47,8 @@ total number of threads needed: ${milkRound.totalThreads}
         while (batchNumber < milkRound.numberOfBatches && availableHostThreads > milkRound.totalBatchThreads) {
             milkRound.instances.forEach(inst => spawnBatchScript(ns, host, inst, batchNumber));
             batchNumber += 1;
-            availableHostThreads -= milkRound.totalBatchThreads;
             await ns.sleep(milkRound.batchOffset);
+            availableHostThreads = numThreads(ns, host, '/batch/grow.js');
         }
     }
 }
