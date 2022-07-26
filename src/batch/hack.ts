50dc3beb62d6e0081e2c6cc4de90f8b0e73a2fd9
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-25 21:23:30 -0700

    [batch] Add optional looping to all instance scripts

--- src/batch/hack.ts
+++ src/batch/hack.ts
@@ -1,23 +1,31 @@
 import type { NS } from "netscript";
 
 export async function main(ns: NS) {
-    const target = ns.args[0];
+    const options = ns.flags([
+        ['loop', false]
+    ]);
+
+    const args = options._;
+
+    const target = args[0];
     if (typeof target != 'string') {
         ns.tprint('invalid target: %s', target);
         return;
     }
 
-    let sleepTime = ns.args[1];
+    let sleepTime = args[1];
     if (typeof sleepTime != 'number') {
         sleepTime = 0;
     }
 
-    let endDelay = ns.args[2];
+    let endDelay = args[2];
     if (typeof endDelay != 'number') {
         endDelay = 0;
     }
 
-    await ns.sleep(sleepTime);
-    await ns.hack(target);
-    await ns.sleep(endDelay);
+    do {
+        await ns.sleep(sleepTime);
+        await ns.hack(target);
+        await ns.sleep(endDelay);
+    } while (options.loop);
 }
