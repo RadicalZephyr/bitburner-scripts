07203d62a2c0818ebdd11821132162bf51976d94
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-26 17:25:44 -0700

    [lib] Add max RAM safeguard to purchase ram function

--- src/lib.ts
+++ src/lib.ts
@@ -676,7 +676,7 @@ export function getHighestPurchasableRamLevel(ns: NS, percentageSpend: number): 
 
     let ram = 16;
 
-    while (maxPerServerSpend > ns.getPurchasedServerCost(ram)) {
+    while (ram * 2 <= maxServerRam && maxPerServerSpend > ns.getPurchasedServerCost(ram)) {
         ram *= 2;
     }
 
