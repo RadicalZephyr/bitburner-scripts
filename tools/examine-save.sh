c6de15fdda42b0763b32b7c3dddc40ed12aec1f8
Zefira Shannon <earthlingzephyr@gmail.com>
2022-07-29 22:00:24 -0700

    [tools] Add a cat of stdin so save examiner works

--- tools/examine-save.sh
+++ tools/examine-save.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jq '.data.AllServersSave | fromjson | to_entries | map({ server: .key , scripts: .value.data.runningScripts | map(.data | {filename, args}) })' | bat
+cat - | jq '.data.AllServersSave | fromjson | to_entries | map({ server: .key , scripts: .value.data.runningScripts | map(.data | {filename, args}) })' | bat
