754e9df8c8abf46597094faaa860dbad49d2f213
Zefira Shannon <earthlingzephyr@gmail.com>
2022-08-10 16:48:49 -0700

    [batch] Restructure halt to improve timing characteristics
    Rather than build long timeouts into the stopping of each target, the
    halting proceeds in phases. First all scripts in the milk category are
    killed for each target, then build, then hack, grow and weaken.
    
    In between each phase once all those scripts have been killed we wait
    for a long period of time to give the scripts a chance to actually end
    and some time for the follower scripts in that batch to also complete.

--- src/batch/halt.ts
+++ src/batch/halt.ts
@@ -24,29 +24,40 @@ OPTIONS
     }
 
     const allHosts = getAllHosts(ns);
-    const allTargetThreads = countThreadsByTarget(ns, allHosts);
+    let allTargetThreads = countThreadsByTarget(ns, allHosts);
 
-    for (const target of options._) {
+    let targets = options._.filter((t: any) => typeof (t) === 'string' && allTargetThreads.get(t));
+
+    for (const target of targets) {
         const targetInfo = allTargetThreads.get(target);
-        if (targetInfo) {
-            const milkPid = targetInfo.hPid.shift();
-            ns.kill(milkPid);
-            await ns.sleep(500);
+        const milkPid = targetInfo.hPid.shift();
+        if (milkPid) ns.kill(milkPid);
+        const buildPid = targetInfo.hPid.shift();
+        if (buildPid) ns.kill(buildPid);
+        await ns.sleep(50);
+    }
+    await ns.sleep(1000);
 
-            targetInfo.hPid.forEach(pid => ns.kill(pid));
-            await ns.sleep(500);
+    allTargetThreads = countThreadsByTarget(ns, allHosts);
+    targets = options._.filter((t: any) => typeof (t) === 'string' && allTargetThreads.get(t));
 
-            const buildPid = targetInfo.hPid.shift();
-            ns.kill(buildPid);
-            await ns.sleep(500);
+    for (const target of targets) {
+        const targetInfo = allTargetThreads.get(target);
+        targetInfo.hPid.forEach(pid => ns.kill(pid));
+        await ns.sleep(50);
+    }
+    await ns.sleep(1000);
 
-            targetInfo.gPid.forEach(pid => ns.kill(pid));
-            await ns.sleep(500);
+    for (const target of targets) {
+        const targetInfo = allTargetThreads.get(target);
+        targetInfo.gPid.forEach(pid => ns.kill(pid));
+        await ns.sleep(50);
+    }
+    await ns.sleep(1000);
 
-            targetInfo.wPid.forEach(pid => ns.kill(pid));
-            await ns.sleep(10);
-        } else {
-            ns.tprintf('nothing to kill for target %s', target);
-        }
+    for (const target of targets) {
+        const targetInfo = allTargetThreads.get(target);
+        targetInfo.wPid.forEach(pid => ns.kill(pid));
+        await ns.sleep(10);
     }
 }
