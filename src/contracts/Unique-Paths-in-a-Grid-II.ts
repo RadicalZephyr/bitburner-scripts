a7e08df02e517c58b8fad53141ce5d007ed65ed5
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-23 12:24:29 -0700

    [contracts] Document Unique Paths in a Grid II

--- src/contracts/Unique-Paths-in-a-Grid-II.ts
+++ src/contracts/Unique-Paths-in-a-Grid-II.ts
@@ -1,8 +1,26 @@
-/*
+/* Unique Paths in a Grid II
+
+You are located in the top-left corner of the following grid:
+
+0,1,0,1,0,0,0,0,0,
+0,0,1,0,0,0,0,0,0,
+1,0,0,0,1,0,0,0,0,
+0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,1,0,
+0,1,0,0,1,0,0,0,0,
+
+You are trying reach the bottom-right corner of the grid, but you can
+only move down or right on each step. Furthermore, there are obstacles
+on the grid that you cannot move onto. These obstacles are denoted by
+'1', while empty spaces are denoted by 0.
+
+Determine how many unique paths there are from start to finish.
+
+NOTE: The data returned for this contract is an 2D array of numbers
+representing the grid.
  */
 
 import type { NS } from "netscript";
-import type { ContractData } from '../all-contracts';
 
 export async function main(ns: NS) {
     let scriptName = ns.getScriptName();
