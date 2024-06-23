d5a30609f408fd0af55d0f07a201c35e13f88a32
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-22 21:57:56 -0700

    [get-contracts] Rewrite to get contract type, data and run solver

--- src/get-contracts.ts
+++ src/get-contracts.ts
@@ -1,25 +1,56 @@
 import type { NS } from "netscript";
+import type { ContractData } from "./all-contracts";
 
-import { CONTRACTS } from './contract-locations';
+import { walkNetworkBFS } from "./util/walk";
 
 export async function main(ns: NS) {
-    let contracts = [];
-    for (let contract of CONTRACTS) {
-        let contractType = ns.codingcontract.getContractType(contract.file, contract.host).replace(':', '').replaceAll(' ', '-');
-        contract['data'] = ns.codingcontract.getData(contract.file, contract.host);
-        contracts.push(contract);
+    let network = walkNetworkBFS(ns);
+    let allHosts = Array.from(network.keys());
 
-        let contractScriptName = ns.sprintf('contracts/%s.js', contractType)
-        if (!ns.fileExists(contractScriptName)) {
-            ns.tprintf('no script found for contract type %s from host %s', contractType, contract['host']);
-            continue;
-        }
+    const contractPortNum = 266; // "CON"
+    const contractFile = /\.cct/;
 
-        let pid = ns.run(contractScriptName, 1, JSON.stringify(contract));
-        if (pid === 0) {
-            ns.tprintf('failed to run script for contract %s from host %s', contractType, contract['host']);
+    let contractPort = ns.getPortHandle(contractPortNum);
+    let contracts: ContractData[] = [];
+
+    for (const host of allHosts) {
+        if (host == "home") { continue; }
+
+        let files = ns.ls(host).filter(file => contractFile.test(file));
+        for (const file of files) {
+            let contractType = ns.codingcontract.getContractType(file, host).replace(':', '').replaceAll(' ', '-');
+            let data = ns.codingcontract.getData(file, host);
+            let contract: ContractData = { type: contractType, file: file, host: host, data: data, answer: null };
+
+            let contractScriptName = ns.sprintf('/contracts/%s.js', contractType)
+            if (!ns.fileExists(contractScriptName)) {
+                ns.tprintf('no script found for contract type %s from host %s', contractType, contract['host']);
+                continue;
+            }
+
+            let pid = ns.run(contractScriptName, 1, contractPortNum, JSON.stringify(data));
+            if (pid === 0) {
+                ns.tprintf('failed to run script for contract %s from host %s', contractType, contract['host']);
+            }
+
+            await contractPort.nextWrite();
+
+            // Wait until the contract solver script finishes.
+            while (true) {
+                await ns.sleep(100);
+                let scriptInfo = ns.getRunningScript(pid);
+                if (scriptInfo === null) {
+                    break;
+                }
+            }
+
+            contract.answer = contractPort.read();
+
+            contracts.push(contract);
         }
     }
-    ns.tprintf('\ncontracts = %s\n', JSON.stringify(contracts));
-}
 
+    let allContractsFile = "all-contracts.js";
+    let fileData = ns.sprintf('export let CONTRACTS = %s;', JSON.stringify(contracts));
+    ns.write(allContractsFile, fileData, 'w');
+}
