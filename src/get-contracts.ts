b3e4d422881fcd7faaf2002ffc40ab1f2b45d90e
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-22 19:56:57 -0700

    [get-contracts] Run contract solvers

--- src/get-contracts.ts
+++ src/get-contracts.ts
@@ -5,10 +5,20 @@ import { CONTRACTS } from './contract-locations';
 export async function main(ns: NS) {
     let contracts = [];
     for (let contract of CONTRACTS) {
-        contract['type'] = ns.codingcontract.getContractType(contract.file, contract.host);
+        let contractType = ns.codingcontract.getContractType(contract.file, contract.host).replaceAll(' ', '-');
         contract['data'] = ns.codingcontract.getData(contract.file, contract.host);
         contracts.push(contract);
 
+        let contractScriptName = ns.sprintf('contracts/%s.js', contractType)
+        if (!ns.fileExists(contractScriptName)) {
+            ns.tprintf('no script found for contract type %s from host %s', contractType, contract['host']);
+            continue;
+        }
+
+        let pid = ns.run(contractScriptName, 1, JSON.stringify(contract));
+        if (pid === 0) {
+            ns.tprintf('failed to run script for contract %s from host %s', contractType, contract['host']);
+        }
     }
     ns.tprintf('\ncontracts = %s\n', JSON.stringify(contracts));
 }
