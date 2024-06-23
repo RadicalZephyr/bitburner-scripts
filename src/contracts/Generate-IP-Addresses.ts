0393e79694026af7c3b2b5c63e7dd9461e4242b6
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-22 22:28:23 -0700

    [contracts] Update all contract stubs with port and solve fn structure

--- src/contracts/Generate-IP-Addresses.ts
+++ src/contracts/Generate-IP-Addresses.ts
@@ -19,11 +19,23 @@ import type { NS } from "netscript";
 import type { ContractData } from '../all-contracts';
 
 export async function main(ns: NS) {
-    let contractDataJSON = ns.args[0];
-    if (typeof contractDataJSON !== 'string') {
-        ns.tprintf('%s contract run with non-string argument. Must be a JSON string containing file, host and contract data.');
+    let scriptName = ns.getScriptName();
+    let contractPortNum = ns.args[0];
+    if (typeof contractPortNum !== 'number') {
+        ns.tprintf('%s contract run with non-number answer port argument', scriptName);
         return;
     }
-    let contractData: ContractData = JSON.parse(contractDataJSON);
+    let contractDataJSON = ns.args[1];
+    if (typeof contractDataJSON !== 'string') {
+        ns.tprintf('%s contract run with non-string data argument. Must be a JSON string containing file, host and contract data.', scriptName);
+        return;
+    }
+    let contractData: any = JSON.parse(contractDataJSON);
     ns.tprintf('contract data: %s', JSON.stringify(contractData));
+    let answer = solve(contractData);
+    ns.writePort(contractPortNum, JSON.stringify(answer));
+}
+
+function solve(data: any): any {
+    return null;
 }
