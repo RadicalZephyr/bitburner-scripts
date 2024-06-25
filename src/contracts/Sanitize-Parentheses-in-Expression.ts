70ae58d2209629f79666ed6e7a1d1f49e0ae4a0a
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-24 20:26:11 -0700

    [contracts] Document Sanitize Parentheses in Expression

--- src/contracts/Sanitize-Parentheses-in-Expression.ts
+++ src/contracts/Sanitize-Parentheses-in-Expression.ts
@@ -1,8 +1,26 @@
-/*
+/* Sanitize Parentheses in Expression
+
+Given the following string:
+
+```
+((())((a -> ["(())a"]
+```
+
+remove the minimum number of invalid parentheses in order to validate
+the string. If there are multiple minimal ways to validate the string,
+provide all of the possible results. The answer should be provided as
+an array of strings. If it is impossible to validate the string the
+result should be an array with only an empty string.
+
+IMPORTANT: The string may contain letters, not just parentheses. Examples:
+```
+"()())()" -> ["()()()", "(())()"]
+"(a)())()" -> ["(a)()()", "(a())()"]
+")(" -> [""]
+```
  */
 
 import type { NS } from "netscript";
-import type { ContractData } from '../all-contracts';
 
 export async function main(ns: NS) {
     let scriptName = ns.getScriptName();
