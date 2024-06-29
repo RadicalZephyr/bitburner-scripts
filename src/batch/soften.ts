197a939189ff9212220237d467e67de5695d6a06
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-28 21:20:43 -0700

    [batch] Add port and lifetime question to soften script

--- src/batch/soften.ts
+++ src/batch/soften.ts
@@ -3,6 +3,9 @@ import type { NS } from "netscript";
 import { SOFTEN_PORT } from 'util/ports';
 
 export async function main(ns: NS) {
+    let softenPort = ns.getPortHandle(SOFTEN_PORT);
+
+    // When should these scripts quit?
     while (true) {
         let currentLevel = ns.getHackingLevel();
 
