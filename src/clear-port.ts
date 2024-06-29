6599453b3639a3e1e27319914a8ff86dce79cdc0
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-28 23:47:31 -0700

    [clear-port] Enhance clear-port to handle multiple ports or "all"

--- src/clear-port.ts
+++ src/clear-port.ts
@@ -1,11 +1,39 @@
 import type { NS } from "netscript";
 
 export async function main(ns: NS) {
-    let portNum = ns.args[0];
-    if (typeof portNum !== 'number') {
-        ns.tprintf('clear port run with non-number port argument');
+    const flags = ns.flags([
+        ['all', false],
+        ['help', false],
+    ]);
+    if (flags.help || typeof flags.all != 'boolean') {
+        ns.tprint(`USAGE: run ${ns.getScriptName()} [-all] PORT_NUM...
+
+This script clears the ports specified by PORT_NUMs or all ports up to
+PORT_NUM.
+
+OPTIONS
+ --all    Clear all port numbers from 0 up until 99,999 or the first specified port number
+ --help   Displays this help message
+`);
         return;
     }
-    let port = ns.getPortHandle(portNum);
-    port.clear();
+
+    let maxPort = 99999;
+    for (const arg of flags._) {
+        let portNum = Number(arg);
+        if (!Number.isNaN(portNum)) {
+            ns.clearPort(portNum);
+            if (maxPort < portNum) {
+                maxPort = portNum;
+            }
+        }
+        await ns.sleep(10);
+    }
+
+    if (flags.all) {
+        for (let i = 1; i <= maxPort; i++) {
+            ns.clearPort(i);
+            await ns.sleep(10);
+        }
+    }
 }
