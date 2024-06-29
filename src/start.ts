dd96c53fd2e37c0df68421f567b7af4395f273ce
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-28 19:48:52 -0700

    [start] Always run crack-all on n00dles

--- src/start.ts
+++ src/start.ts
@@ -1,15 +1,32 @@
 import type { NS } from "netscript";
 
 export async function main(ns: NS) {
-    await waitForExit(ns, ns.run("/get-all-hosts.js"));
-    // Sleep to let the game fully write out the all-hosts.js script
-    await ns.sleep(1000);
+    await waitForExit(ns, ns.exec("/get-all-hosts.js", "home"));
     ns.tprint("finished fetching all host info");
 
-    await waitForExit(ns, ns.run("/crack-all.js"));
-    ns.tprint("finished cracking all possible hosts");
+    // Sleep to let the game fully write out the all-hosts.js script
+    await ns.sleep(1000);
 
-    // let homeMaxRAM = ns.getServerMaxRam('home');
+    startCracker(ns);
+}
+
+const CRACK_FILES: string[] = [
+    "/all-hosts.js",
+    "/util/ports.js",
+    "/crack-all.js"
+];
+
+function startCracker(ns: NS) {
+    let host = "n00dles";
+    let script = "/crack-all.js";
+    let crackScript = ns.getRunningScript(script, host);
+    if (crackScript !== null) {
+        // TODO: Should we check if the kill succeeded?
+        ns.kill(crackScript.pid);
+    }
+
+    ns.scp(CRACK_FILES, host, "home");
+    ns.exec(script, host);
 }
 
 async function waitForExit(ns: NS, pid: number): Promise<void> {
