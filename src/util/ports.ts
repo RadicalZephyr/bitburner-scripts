4aa0527ec0b0ca9ced6a661aba07bb26bf9ba0e0
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-28 18:31:04 -0700

    [util] Add ports utility program

---
+++ src/util/ports.ts
@@ -0,0 +1,13 @@
+import type { NS } from "netscript";
+
+function toPort(name: string): number {
+    return name.split('').map((s) => s.codePointAt(0)).reduce((p, n) => p + n);
+}
+
+export const SOFTEN_PORT: number = toPort("soften");
+export const GROW_PORT: number = toPort("grow");
+export const STEAL_PORT: number = toPort("steal");
+
+export async function main(ns: NS) {
+    ns.tprintf("Soften Port: %s\nGrow Port: %s\nSteal Port: %s\n", SOFTEN_PORT, GROW_PORT, STEAL_PORT);
+}
