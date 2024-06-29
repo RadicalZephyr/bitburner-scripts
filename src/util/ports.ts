060b9120876236c4a32d69a29161594528390921
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-28 20:06:15 -0700

    [util] Add monitor port number

--- src/util/ports.ts
+++ src/util/ports.ts
@@ -7,7 +7,14 @@ function toPort(name: string): number {
 export const SOFTEN_PORT: number = toPort("soften");
 export const GROW_PORT: number = toPort("grow");
 export const STEAL_PORT: number = toPort("steal");
+export const MONITOR_PORT: number = toPort("monitor");
 
 export async function main(ns: NS) {
-    ns.tprintf("Soften Port: %s\nGrow Port: %s\nSteal Port: %s\n", SOFTEN_PORT, GROW_PORT, STEAL_PORT);
+    ns.tprintf(
+        "Soften Port: %s\nGrow Port: %s\nSteal Port: %s\nMonitor Port: %s\n",
+        SOFTEN_PORT,
+        GROW_PORT,
+        STEAL_PORT,
+        MONITOR_PORT
+    );
 }
