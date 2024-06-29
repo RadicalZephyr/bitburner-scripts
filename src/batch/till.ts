6c5fff2e2766c5340bbf8a597a98e4e55aa5617a
Zefira Shannon <earthlingzephyr@gmail.com>
2024-06-28 23:13:52 -0700

    [batch] Implement a basic greedy tiller

--- src/batch/till.ts
+++ src/batch/till.ts
@@ -1,14 +1,74 @@
 import type { NS } from "netscript";
 
-import { TILL_PORT } from 'util/ports';
+import { EMPTY_SENTINEL, TILL_PORT } from 'util/ports';
+
+const softenScript = "/batch/w.js";
+
+type SoftenPid = {
+    pid: number,
+    target: string,
+};
 
 export async function main(ns: NS) {
-    let tillPort = ns.getPortHandle(TILL_PORT);
+    const serverName = ns.getHostname();
+    const maxRam = ns.getServerMaxRam(serverName);
+    const softenScriptRam = ns.getScriptRam(softenScript, serverName);
+
+    const tillPort = ns.getPortHandle(TILL_PORT);
+
+    let softenPids: SoftenPid[] = [];
 
     // When should these scripts quit?
     while (true) {
-        let currentLevel = ns.getHackingLevel();
-        await ns.sleep(100);
+        let availableRam = maxRam - ns.getServerUsedRam(serverName);
 
+        while (availableRam < softenScriptRam) {
+            await ns.sleep(1000);
+            availableRam = maxRam - ns.getServerUsedRam(serverName);
+        }
+
+        let nextTarget = tillPort.read() as string;
+        if (nextTarget === EMPTY_SENTINEL) {
+            await tillPort.nextWrite();
+            continue;
+        }
+
+        let softenThreads = softenAnalyze(ns, nextTarget);
+        let totalRam = softenScriptRam * softenThreads;
+
+        if (availableRam >= totalRam) {
+            // Enough space is available, just launch the script
+            let pid = ns.run(softenScript, softenThreads, nextTarget);
+            if (pid !== 0) {
+                softenPids.push({ pid: pid, target: nextTarget });
+            }
+        } else {
+            let rounds = Math.ceil(totalRam / availableRam);
+            let maxThreads = Math.floor(availableRam / softenScriptRam);
+
+            let pid = ns.run(softenScript, maxThreads, nextTarget, 1, rounds);
+            if (pid !== 0) {
+                softenPids.push({ pid: pid, target: nextTarget });
+            }
+        }
+
+        await ns.sleep(100);
     }
+}
+
+/** Calculate the number of threads needed to soften the `target` by
+ * the given multiplier.
+ */
+export function softenAnalyze(ns: NS, target: string): number {
+    const currentSecurity = ns.getServerSecurityLevel(target);
+    const minSecurity = ns.getServerMinSecurityLevel(target);
+    return softenThreads(currentSecurity - minSecurity);
+}
+
+/** Calculate the number of threads to soften any server by the given amount.
+ */
+export function softenThreads(softenAmount: number): number {
+    // We multiply by 20 because 1 thread of weaken reduces security
+    // by a fixed amount of 0.05, or 1/20
+    return Math.ceil(softenAmount * 20);
 }
