2fd2661a0ada05624ceea1a0fcd2344818eda074
Zefira Shannon <earthlingzephyr@gmail.com>
2024-08-01 20:14:24 -0700

    [util] Make waitForExit cheaper
    Use `ns.isRunning` instead of `ns.getRunningScript` since we don't
    need to retrieve the pid of the script.

--- src/util/wait.ts
+++ src/util/wait.ts
@@ -3,7 +3,7 @@ import type { NS } from "netscript";
 export async function waitForExit(ns: NS, pid: number): Promise<void> {
     while (true) {
         await ns.sleep(100);
-        if (ns.getRunningScript(pid) === null) {
+        if (!ns.isRunning(pid)) {
             break;
         }
     }
