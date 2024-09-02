f72aad4a5e4f174176595c92dd23229dafed8a86
Zefira Shannon <earthlingzephyr@gmail.com>
2024-09-01 23:07:50 -0700

    [batch] Rewrite manage script to not use Sodium
    I don't think it offers any real advantages and using it requires some
    gymnastics that add more complexity for no benefit.
    
    Too much of what needs to be done requires gathering data from `ns`
    calls, and making any changes also requires side-effects of `ns`
    APIs. But we have to periodically run `ns.sleep` to not freeze the UI,
    and if these `ns` APIs are used in the listeners to Sodium `Streams`
    then there is some overlap that causes an error.

--- src/batch/manage.ts
+++ src/batch/manage.ts
@@ -2,68 +2,37 @@ import type { NetscriptPort, NS, ProcessInfo } from "netscript";
 
 import { HostMsg, WorkerType, TargetType, HOSTS_PORT, HOSTS_DONE, EMPTY_SENTINEL } from "util/ports";
 
-import { Stream, StreamSink, Transaction } from "sodium";
-
 export async function main(ns: NS) {
     ns.tail();
 
     let hostsPort = ns.getPortHandle(HOSTS_PORT);
 
-    const tickSink: StreamSink<void> = new StreamSink();
-
-    const workerSink: StreamSink<Worker> = new StreamSink();
-    const targetSink: StreamSink<Target> = new StreamSink();
+    let newWorkers = [];
+    let newTargets = [];
 
     let hostsMessagesWaiting = true;
 
-    setup(workerSink, targetSink, tickSink, ns);
-
-    let streamHostsToSink = makeStreamHostsToSink(ns, hostsPort, workerSink, targetSink);
+    let readHostsFromPort = makeReadHostsFromPort(ns, hostsPort, newWorkers, newTargets);
 
     while (true) {
         if (hostsMessagesWaiting) {
-            streamHostsToSink();
+            readHostsFromPort();
             hostsMessagesWaiting = false;
 
             hostsPort.nextWrite().then(_ => {
                 hostsMessagesWaiting = true;
             });
         }
-        tickSink.send(null);
+        await tick(ns);
         await ns.sleep(100);
     }
 }
 
-/** Configure the Sodium event graph.
- *
- * DO NOT add a `ns: NS` parameter. Using `ns` APIs inside Sodium
- * callbacks is likely to result in "failed concurrency" errors if
- * they happen to run at the same time as `ns.sleep` in the main loop.
- */
-function setup(workerStream: Stream<Worker>, targetStream: Stream<Target>, tickStream: Stream<void>, ns: NS): void {
-    Transaction.run(() => {
-        let emptyWorkerList: Worker[] = [];
-        let workersCell = workerStream.accum(emptyWorkerList, (w: Worker, workers: Worker[]) => [...workers, w]);
-
-        let emptyTargetList: Target[] = [];
-        let targetsCell = targetStream.accum(emptyTargetList, (t: Target, targets: Target[]) => [...targets, t]);
-
-
-        // ########################################################
-        // ## Listeners
-        // ########################################################
-
-        tickStream.listen(() => ns.printf("management tick"));
-        workerStream.listen((w: Worker) => ns.printf("new worker: %s", w.name));
-        targetStream.listen((t: Target) => ns.printf("new target: %s", t.name));
-    });
-}
-
-function makeStreamHostsToSink(
+function makeReadHostsFromPort(
     ns: NS,
     hostsPort: NetscriptPort,
-    workerSink: StreamSink<Worker>,
-    targetSink: StreamSink<Target>,
+    newWorker: Worker[],
+    newTarget: Target[],
 ) {
     return function() {
         // Read everything from the port until empty or getting the done signal.
@@ -77,10 +46,10 @@ function makeStreamHostsToSink(
                 let nextHostMsg = nextMsg as HostMsg;
                 switch (nextHostMsg.type) {
                     case WorkerType:
-                        workerSink.send(new Worker(ns, nextHostMsg.host));
+                        newWorker.push(new Worker(ns, nextHostMsg.host));
                         break;
                     case TargetType:
-                        targetSink.send(new Target(ns, nextHostMsg.host));
+                        newTarget.push(new Target(ns, nextHostMsg.host));
                         break;
                 }
             }
@@ -114,4 +83,8 @@ class Target {
         this.maxMoney = ns.getServerMaxMoney(host);
         this.minSec = ns.getServerMinSecurityLevel(host);
     }
+}
+
+async function tick(ns: NS) {
+
 }
