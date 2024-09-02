f2a2e55e54847cdb88b56124be9f6c969f11bb62
Zefira Shannon <earthlingzephyr@gmail.com>
2024-09-02 04:36:48 -0700

    [batch] Store the required hack level on Targets

--- src/batch/manage.ts
+++ src/batch/manage.ts
@@ -91,12 +91,14 @@ class Worker {
 class Target {
     ns: NS;
     name: string;
+    hckLevel: number;
     maxMoney: number;
     minSec: number;
 
     constructor(ns: NS, host: string) {
         this.ns = ns;
         this.name = host;
+        this.hckLevel = ns.getServerRequiredHackingLevel(host);
         this.maxMoney = ns.getServerMaxMoney(host);
         this.minSec = ns.getServerMinSecurityLevel(host);
     }
