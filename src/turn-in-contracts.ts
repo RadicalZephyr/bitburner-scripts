2e8008287577e7d5197ae8cee7de721dd23b08bb
Zefira Shannon <zefira@hey.com>
2025-06-21 06:36:35 -0700

    [turn-in-contracts] Record and report contract failures

--- src/turn-in-contracts.ts
+++ src/turn-in-contracts.ts
@@ -3,12 +3,29 @@ import type { NS } from "netscript";
 import { CONTRACTS } from "all-contracts";
 
 export async function main(ns: NS) {
+    let failures = {};
+
     for (const contract of CONTRACTS) {
         if (contract.answer !== "null") {
             let reward = ns.codingcontract.attempt(contract.answer, contract.file, contract.host);
             if (reward) {
                 ns.tprintf("solved %s, received %s", contract.type, reward);
+            } else {
+                if (failures[contract.type]) {
+                    failures[contract.type].push(contract);
+                } else {
+                    failures[contract.type] = [contract];
+                }
             }
+        }
+    }
+
+    for (const contractType in failures) {
+        let contractFailures = failures[contractType];
+        ns.tprintf("failed %s contracts of type %s:", contractFailures.length);
+
+        for (const contract of contractFailures) {
+            ns.tprintf(" failed %s on host %s\ndata: %s\nanswer: %s", contract.file, contract.host, contract.data, contract.answer);
         }
     }
 }
