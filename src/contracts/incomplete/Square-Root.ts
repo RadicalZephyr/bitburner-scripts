b996511ca5d260b56a5c3b12699d0f10cf4776dc
Zefira Shannon <zefira@hey.com>
2025-06-23 16:29:16 -0700

    [contracts] Use Heron's method to solve Square Root contracts

--- src/contracts/incomplete/Square-Root.ts
+++ src/contracts/incomplete/Square-Root.ts
@@ -27,17 +27,48 @@ export async function main(ns: NS) {
         ns.tprintf('%s contract run with non-number answer port argument', scriptName);
         return;
     }
+
     let contractDataJSON = ns.args[1];
     if (typeof contractDataJSON !== 'string') {
         ns.tprintf('%s contract run with non-string data argument. Must be a JSON string of the contract data.', scriptName);
         return;
     }
-    let contractData: BigInt = BigInt(contractDataJSON);
-    ns.tprintf('contract data: %s', contractDataJson.toString());
+    // N.B. In order to prevent BitBurner from automatically
+    // destroying the bigint by parsing it as a number and truncating
+    // it, the numeric string needs to be double quoted, so the actual
+    // string we read contains quotation marks that need to be
+    // stripped. We could use JSON.parse for this, but it seems
+    // simpler to just directly strip the quotation marks.
+    let contractData: BigInt = BigInt(contractDataJSON.substring(1, contractDataJSON.length - 1));
+    ns.tprintf('contract data: %s', contractData.toString());
     let answer = solve(contractData);
-    ns.writePort(contractPortNum, JSON.stringify(answer));
+    ns.writePort(contractPortNum, answer.toString());
 }
 
-function solve(data: BigInt): any {
-    return null;
+function solve(data: BigInt): BigInt {
+    let S = data;
+    let s = S.toString();
+
+    // Base an estimate on the square root as such `S = a * (10 **
+    // 2n)` which implies that `sqrt(S) = sqrt(a) * (10 ** n)`, where
+    // `1 <= a < 100`
+
+    // Calculate sqrt(a), where a is the two most significant digits
+    // of s
+    let a = Math.round(Math.sqrt(JSON.parse(s.substring(0, 2))));
+    // Calculate n from  for the exponent
+    let n = Math.floor((s.length - 2) / 2);
+
+    let x_n = BigInt(a * (10 ** n));
+
+    // Now iteratively calculate better approximations to the square
+    // root of S using Heron's Method
+    let two = BigInt(2);
+
+    while (!(x_n * x_n < S && (x_n + BigInt(1)) * (x_n + BigInt(1)) > S)) {
+        let x_n1 = (x_n + (S / x_n)) / two;
+        x_n = x_n1;
+    }
+
+    return x_n;
 }
