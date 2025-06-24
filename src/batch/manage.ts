5e758bc4d9869771731d60e0fd6452a2fe77bc01
Zefira Shannon <zefira@hey.com>
2025-06-24 02:44:20 -0700

    [batch] Remove old unused Manage code

--- src/batch/manage.ts
+++ src/batch/manage.ts
@@ -89,37 +89,12 @@ class State {
     }
 
     readyToTillTargets(): Target[] {
-        function byHckLevelAndMoney(a: Target, b: Target): number {
-            let hckDiff = a.hckLevel - b.hckLevel;
-            return hckDiff != 0 ? hckDiff : a.maxMoney - b.maxMoney;
-        }
-
-        let hckLevel = this.ns.getHackingLevel();
-        return this.pendingTargets
-            .filter(target => hckLevel >= target.hckLevel)
-            .sort(byHckLevelAndMoney);
+        return [];
     }
 
     tillNewTargets() {
-        // let readyToTillTargets = this.readyToTillTargets();
-
-        // if (this.tillTargets.length < this.options.maxTillTargets) {
-        //     let newTargetsCount = this.options.maxTillTargets - this.tillTargets.length;
-        //     for (let i = 0; i < newTargetsCount; i++) {
-        //         let sTarget = readyToTillTargets.shift();
-        //         let weakenInstance = new WeakenInstance(this.ns, sTarget);
-        //         // this.spawnScriptInstance(weakenInstance);
-        //     }
-        // }
     }
 
-    // spawnScriptInstance(scriptInstance: BatchScriptInstance) {
-    //     while (scriptInstance.needsMoreThreads()) {
-    //         let nextWorker = this.workers.shift();
-    //         nextWorker.update();
-    //         spawnScriptOnWorker(this.ns, nextWorker, scriptInstance);
-    //     }
-    // }
 }
 
 async function tick(ns: NS, state: State) {
