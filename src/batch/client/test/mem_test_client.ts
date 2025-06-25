af677d5d676166c3b2eb179a7c5d505195200768
Zefira Shannon <zefira@hey.com>
2025-06-25 02:34:08 -0700

    [batch] Update mem_test_client to test partial release

--- src/batch/client/test/mem_test_client.ts
+++ src/batch/client/test/mem_test_client.ts
@@ -3,8 +3,22 @@ import type { NS } from "netscript";
 import { MemoryClient } from "/batch/client/memory";
 
 export async function main(ns: NS) {
-    let client = new MemoryClient(ns);
-    let response = await client.requestOwnedAllocation(1, 1);
-    ns.tprintf("got allocation response: %s", JSON.stringify(response));
+    const client = new MemoryClient(ns);
 
+    const allocation = await client.requestTransferableAllocation(1, 3);
+    if (!allocation) {
+        ns.tprintf("allocation failed");
+        return;
+    }
+
+    allocation.releaseAtExit(ns, "mem-test");
+    ns.tprintf("initial allocation: %s", JSON.stringify(allocation));
+    await ns.sleep(3000);
+
+    const partial = await client.releaseChunks(allocation.allocationId, 1);
+    ns.tprintf("after releasing one chunk: %s", JSON.stringify(partial));
+    await ns.sleep(3000);
+
+    const final = await client.releaseChunks(allocation.allocationId, 2);
+    ns.tprintf("after releasing remaining chunks: %s", JSON.stringify(final));
 }
