3460f5252ab3c2e54575ddd80be828d8845fb3fe
Zefira Shannon <zefira@hey.com>
2025-06-24 18:35:00 -0700

    [batch] Fill out help and arg parsing boilerplate for harvest script

--- src/batch/harvest.ts
+++ src/batch/harvest.ts
@@ -1,4 +1,48 @@
 import type { NS } from "netscript";
 
+import { registerAllocationOwnership } from "./client/memory";
+
 export async function main(ns: NS) {
+    ns.disableLog('ALL');
+
+    const flags = ns.flags([
+        ['allocation-id', -1],
+        ['help', false],
+    ]);
+
+    const rest = flags._ as string[];
+    if (rest.length === 0 || flags.help) {
+        ns.tprint(`
+USAGE: run ${ns.getScriptName()} SERVER_NAME
+
+Launch as many grow and weaken threads as needed to maximize money
+of SERVER_NAME while keeping security at a minimum.
+
+Example:
+> run ${ns.getScriptName()} n00dles
+
+OPTIONS
+--help           Show this help message
+`);
+        return;
+    }
+
+    let allocationId = flags['allocation-id'];
+    if (allocationId !== -1) {
+        if (typeof allocationId !== 'number') {
+            ns.tprint('--allocation-id must be a number');
+            return;
+        }
+        registerAllocationOwnership(ns, allocationId);
+    }
+
+    let target = rest[0];
+    if (typeof target !== 'string' || !ns.serverExists(target)) {
+        ns.tprintf("target %s does not exist", target);
+        return;
+    }
+
+    ns.ui.setTailTitle(`harvest ${target}`);
+    ns.ui.openTail();
+    ns.ui.resizeTail(400, 80);
 }
