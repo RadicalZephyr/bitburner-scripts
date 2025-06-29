454a4d00b26ab3f96e7fb689ebd1ab38390c3603
Zefira Shannon <zefira@hey.com>
2025-06-29 11:25:52 -0700

    [list-backdoors] Remove home and personal servers from list

--- src/list-backdoors.tsx
+++ src/list-backdoors.tsx
@@ -64,7 +64,7 @@ export async function main(ns: NS) {
 
         for (const host of network.keys()) {
             const info = ns.getServer(host);
-            if (!info.backdoorInstalled) {
+            if (!(info.hostname === "home" || info.purchasedByPlayer || info.backdoorInstalled)) {
                 missingBackdoor.push(host);
             }
         }
