d16661bb4c370be31b350cf40042cf47eab8c95c
Zefira Shannon <zefira@hey.com>
2025-06-29 23:25:01 -0700

    [bootstrap] Use a more prosaic bootstrap until dynamicImport stabilizes

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -13,7 +13,22 @@ interface ImportedScript {
     main: (ns: NS) => Promise<void>;
 }
 
-export async function main(_ns: NS) {
+export async function main(ns: NS) {
+    for (const script of BOOTSTRAP_SCRIPTS) {
+        let pid = ns.run(script);
+        if (pid === 0) {
+            reportError(ns, `failed to launch ${script}`);
+        }
+    }
+}
+
+function reportError(ns: NS, error: string) {
+    ns.toast(error, "error");
+    ns.print(`ERROR: ${error}`);
+    ns.ui.openTail();
+}
+
+export async function dynamicBootstrap(_ns: NS) {
     let ns = _ns as DynImportNS;
 
     let currentDynamicRam = ns.ramOverride();;
