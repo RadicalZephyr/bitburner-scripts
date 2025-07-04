1a2783724676e59c95b799f2dfe36fd88938e99c
Zefira Shannon <zefira@hey.com>
2025-07-03 19:11:23 -0700

    [services] Add a basic services config

---
+++ src/services/config.ts
@@ -0,0 +1,15 @@
+import { LocalStorage, setConfigDefault } from "util/localStorage";
+
+const SUB_MAX_RETRIES = "DISCOVERY_SUB_MAX_RETRIES";
+
+class Config {
+    setDefaults() {
+        setConfigDefault(SUB_MAX_RETRIES, (5).toString());
+    }
+
+    get subscriptionMaxRetries() {
+        return Number(LocalStorage.getItem(SUB_MAX_RETRIES));
+    }
+}
+
+export const CONFIG = new Config();
