1fd43d6257fba84ce48f9a0ec1f1fd031fc8667c
Zefira Shannon <zefira@hey.com>
2025-07-04 18:57:28 -0700

    [util] Add array extend function in utils

---
+++ src/util/extend.ts
@@ -0,0 +1,16 @@
+/** Efficiently extend `array` with the given `values`. */
+export function extend<T>(array: T[], values: T[]): T[] {
+    var l2 = values.length;
+
+    if (l2 === 0)
+        return array;
+
+    var l1 = array.length;
+
+    array.length += l2;
+
+    for (var i = 0; i < l2; i++)
+        array[l1 + i] = values[i];
+
+    return array;
+};
