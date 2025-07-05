93c93a1539418aded91c6c994650f2cf4db5b245
Zefira Shannon <zefira@hey.com>
2025-07-05 16:01:23 -0700

    [stopworld] Hopefully improve performance of stopworld by putting all target scripts in a Set

--- src/stopworld.ts
+++ src/stopworld.ts
@@ -23,13 +23,13 @@ Example:
         return;
     }
 
-    const targetScripts = flags._ as string[];
+    const targetScripts = new Set(flags._ as string[]);
 
     const networkGraph = walkNetworkBFS(ns);
     for (const host of networkGraph.keys()) {
-        if (targetScripts.length > 0) {
+        if (targetScripts.size > 0) {
             ns.ps(host)
-                .filter(pi => targetScripts.includes(pi.filename))
+                .filter(pi => targetScripts.has(pi.filename))
                 .forEach(pi => ns.kill(pi.pid));
         } else {
             ns.killall(host, true);
