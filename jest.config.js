1f045ce801c60ed80895b3733cfb7ef9181ab887
Zefira Shannon <zefira@hey.com>
2025-07-06 11:26:51 -0700

    Use recommended modern way to use ts-jest preset

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
 const { createDefaultPreset } = require("ts-jest");
 
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
-    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     moduleNameMapper: {
         '^(.*)\\.js$': '$1',
     },
-    transform: {
-        ...tsJestTransformCfg,
-    },
     testMatch: ['<rootDir>/src/**/*.test.ts'],
+    ...createDefaultPreset()
 };
