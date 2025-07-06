04ae5c2f5303c6bf764132e957e3c81edf2d3640
Zefira Shannon <zefira@hey.com>
2025-07-06 10:35:28 -0700

    Reindent watch file

--- build/watch.js
+++ build/watch.js
@@ -7,7 +7,7 @@ const { src, dist, allowedFiletypes } = require('./config');
 
 /** Format dist path for printing */
 function normalize(p) {
-  return p.replace(/\\/g, '/');
+    return p.replace(/\\/g, '/');
 }
 
 /**
@@ -15,30 +15,30 @@ function normalize(p) {
  * Include init and watch phase.
  */
 async function syncStatic() {
-  return syncDirectory.async(path.resolve(src), path.resolve(dist), {
-    exclude: (file) => {
-      const { ext } = path.parse(file);
-      return ext && !allowedFiletypes.includes(ext);
-    },
-    async afterEachSync(event) {
-      // log file action
-      let eventType;
-      if (event.eventType === 'add' || event.eventType === 'init:copy') {
-        eventType = 'changed';
-      } else if (event.eventType === 'unlink') {
-        eventType = 'deleted';
-      }
-      if (eventType) {
-        let relative = event.relativePath;
-        if (relative[0] === '\\') {
-          relative = relative.substring(1);
-        }
-        console.log(`${normalize(relative)} ${eventType}`);
-      }
-    },
-    watch: true,
-    deleteOrphaned: true,
-  });
+    return syncDirectory.async(path.resolve(src), path.resolve(dist), {
+        exclude: (file) => {
+            const { ext } = path.parse(file);
+            return ext && !allowedFiletypes.includes(ext);
+        },
+        async afterEachSync(event) {
+            // log file action
+            let eventType;
+            if (event.eventType === 'add' || event.eventType === 'init:copy') {
+                eventType = 'changed';
+            } else if (event.eventType === 'unlink') {
+                eventType = 'deleted';
+            }
+            if (eventType) {
+                let relative = event.relativePath;
+                if (relative[0] === '\\') {
+                    relative = relative.substring(1);
+                }
+                console.log(`${normalize(relative)} ${eventType}`);
+            }
+        },
+        watch: true,
+        deleteOrphaned: true,
+    });
 }
 
 /**
@@ -46,20 +46,20 @@ async function syncStatic() {
  * Init phase only.
  */
 async function initTypeScript() {
-  const distFiles = await fg(`${dist}/**/*.js`);
-  for (const distFile of distFiles) {
-    // search existing *.js file in dist
-    const relative = path.relative(dist, distFile);
-    const srcFile = path.resolve(src, relative);
-    // if srcFile does not exist, delete distFile
-    if (
-      !fs.existsSync(srcFile) &&
-      !fs.existsSync(srcFile.replace(/\.js$/, '.ts'))
-    ) {
-      await fs.promises.unlink(distFile);
-      console.log(`${normalize(relative)} deleted`);
+    const distFiles = await fg(`${dist}/**/*.js`);
+    for (const distFile of distFiles) {
+        // search existing *.js file in dist
+        const relative = path.relative(dist, distFile);
+        const srcFile = path.resolve(src, relative);
+        // if srcFile does not exist, delete distFile
+        if (
+            !fs.existsSync(srcFile) &&
+            !fs.existsSync(srcFile.replace(/\.js$/, '.ts'))
+        ) {
+            await fs.promises.unlink(distFile);
+            console.log(`${normalize(relative)} deleted`);
+        }
     }
-  }
 }
 
 /**
@@ -67,16 +67,16 @@ async function initTypeScript() {
  * Watch phase only.
  */
 async function watchTypeScript() {
-  chokidar.watch(`${src}/**/*.ts`).on('unlink', async (p) => {
-    // called on *.ts file get deleted
-    const relative = path.relative(src, p).replace(/\.ts$/, '.js');
-    const distFile = path.resolve(dist, relative);
-    // if distFile exists, delete it
-    if (fs.existsSync(distFile)) {
-      await fs.promises.unlink(distFile);
-      console.log(`${normalize(relative)} deleted`);
-    }
-  });
+    chokidar.watch(`${src}/**/*.ts`).on('unlink', async (p) => {
+        // called on *.ts file get deleted
+        const relative = path.relative(src, p).replace(/\.ts$/, '.js');
+        const distFile = path.resolve(dist, relative);
+        // if distFile exists, delete it
+        if (fs.existsSync(distFile)) {
+            await fs.promises.unlink(distFile);
+            console.log(`${normalize(relative)} deleted`);
+        }
+    });
 }
 
 /**
@@ -84,8 +84,8 @@ async function watchTypeScript() {
  * Include init and watch phase.
  */
 async function syncTypeScript() {
-  await initTypeScript();
-  return watchTypeScript();
+    await initTypeScript();
+    return watchTypeScript();
 }
 
 console.log('Start watching static and ts files...');
