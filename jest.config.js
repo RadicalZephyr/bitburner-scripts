ef120bdd7bb428ade3947e3733424fd3ca55cc07
Zefira Shannon <zefira@hey.com>
2025-07-07 12:51:21 -0700

    [jest] Add src directory to the module paths so our root relative imports can resolve
    Our imports are a bit weird for node resolution rules to make them work in Bitburner.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const { createDefaultPreset } = require("ts-jest");
 /** @type {import('jest').Config} */
 module.exports = {
     testEnvironment: 'node',
+    modulePaths: ['<rootDir>/src/'],
     moduleNameMapper: {
         '^(util/.*)$': '<rootDir>/src/$1',
         '^(.*)\\.js$': '$1',
