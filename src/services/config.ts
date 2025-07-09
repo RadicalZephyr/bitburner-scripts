97d398a2fd3619fa7f85036f0b77218b50617b90
Zefira Shannon <zefira@hey.com>
2025-07-09 06:55:23 -0700

    [services] Add exec retrying to GrowableAllocation.launch

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,9 @@
 import { Config, ConfigInstance } from "util/config";
 
-const entries = [["subscriptionMaxRetries", 5]] as const;
+const entries = [
+    ["subscriptionMaxRetries", 5],
+    ["launchRetryMax", 5]
+] as const;
 
 export const CONFIG: ConfigInstance<typeof entries> =
     new Config("DISCOVERY", entries) as ConfigInstance<typeof entries>;
