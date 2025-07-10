355392889df0712a240060da340cc009c2e6279a
Zefira Shannon <zefira@hey.com>
2025-07-10 02:45:57 -0700

    [util] Add a utility for non-ns based time functions

---
+++ src/util/time.ts
@@ -0,0 +1,48 @@
+type TimerHandler = string | Function;
+
+type SetInterval = (handler: TimerHandler, timeout?: number, ...args: any[]) => number;
+type SetTimeout = (handler: TimerHandler, timeout?: number, ...args: any[]) => number;
+
+interface Timing {
+    setInterval: SetInterval;
+    setTimeout: SetTimeout;
+}
+
+declare global {
+    var globalThis: Timing;
+}
+
+/**
+ * Sleep for at least ms milliseconds.
+ *
+ * @param ms - Number of milliseconds to sleep for
+ * @returns A Promise that resolves after the sleep has elapsed
+ */
+export function sleep(ms: number): Promise<void> {
+    return new Promise(res => globalThis.setTimeout(ms, () => res.call(null)));
+}
+
+/**
+ * Repeatedly calls a function or executes a code snippet, with a
+ * fixed time delay between each call.
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/setInterval)
+ *
+ * @param handler - A function to be executed after the timer expires.
+ * @param timeout - The time, in milliseconds that the timer should wait before the specified function is executed. If this parameter is omitted, a value of 0 is used, meaning execute "immediately", or more accurately, the next event cycle.
+ * @param args    - Arguments to pass to the function
+ */
+export const setInterval: SetInterval = globalThis.setInterval;
+
+
+/**
+ * Sets a timer which executes a function or specified piece of code
+ * once the timer expires.
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/setTimeout)
+ *
+ * @param handler - A function to be executed after the timer expires.
+ * @param timeout - The time, in milliseconds that the timer should wait before the specified function is executed. If this parameter is omitted, a value of 0 is used, meaning execute "immediately", or more accurately, the next event cycle.
+ * @param args    - Arguments to pass to the function
+ */
+export const setTimeout: SetTimeout = globalThis.setTimeout;
