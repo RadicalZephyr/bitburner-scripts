726f111607798340e11cd20aa5cc67aed4efa6f6
Zefira Shannon <zefira@hey.com>
2025-07-13 16:13:01 -0700

    [corp] Add a stop button to eat script

--- src/corp/eat.tsx
+++ src/corp/eat.tsx
@@ -1,4 +1,4 @@
-import type { NS } from "netscript";
+import type { NS, UserInterfaceTheme } from "netscript";
 
 declare var React: any;
 
@@ -8,8 +8,9 @@ export async function main(ns: NS) {
     ns.ui.openTail();
 
     while (true) {
+        const theme = ns.ui.getTheme();
         ns.clearLog();
-        ns.printRaw(<EatIt />);
+        ns.printRaw(<EatIt theme={theme} />);
         ns.ui.renderTail();
 
         await ns.sleep(1000);
@@ -17,6 +18,7 @@ export async function main(ns: NS) {
 }
 
 let eating = false;
+let intervalId: number;
 
 function startEating() {
     if (eating) return;
@@ -25,7 +27,7 @@ function startEating() {
 
     const eatButton = findEatNoodlesButton();
 
-    let intervalId: number;
+
     intervalId = globalThis.setInterval(() => {
         if (eatButton) {
             eatButton.click();
@@ -47,13 +49,24 @@ function findEatNoodlesButton() {
     return null;
 }
 
-interface IEatItProps { }
+function stopEating() {
+    if (typeof intervalId !== "number") return;
 
-function EatIt({ }: IEatItProps) {
+    globalThis.clearInterval(intervalId);
+    intervalId = null;
+}
+
+interface IEatItProps {
+    theme: UserInterfaceTheme;
+}
+
+function EatIt({ theme }: IEatItProps) {
+    const buttonStyle = { backgroundColor: theme.well, padding: "2em", margin: "0.5em" };
     return (
         <>
             <h1>Eat All The Noodles!</h1>
-            <a href="#" onClick={() => startEating()}>Eat it!</a>
+            <a href="#" style={{ color: theme.successlight, ...buttonStyle }} onClick={() => startEating()}>Eat it!</a>
+            <a href="#" style={{ color: theme.errorlight, ...buttonStyle }} onClick={() => stopEating()}>STOP!</a>
         </>
     );
 }
