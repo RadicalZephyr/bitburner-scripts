50772e52733d8988cfd480d2c15a3b16b4c491ef
Zefira Shannon <zefira@hey.com>
2025-07-17 00:00:16 -0700

    [gang] Only buy 1 piece of equipment per member per tick
    This makes the equipment distribution slightly fairer because every
    tick every gang member gets a chance to buy equipment and earlier
    members don't get to spend the entire budget on all their equipment.

--- src/gang/equipment-manager.ts
+++ src/gang/equipment-manager.ts
@@ -37,21 +37,51 @@ export function purchaseBestGear(
     profile: RoleProfile,
 ) {
     const info: GangMemberInfo = ns.gang.getMemberInformation(memberName);
-    const equips = ns.gang.getEquipmentNames();
     const limit = CONFIG.maxROITime[role] ?? 0;
     const gainRate = moneyTracker.velocity("total");
     if (!gainRate) return;
 
+    const equips = ns.gang.getEquipmentNames().map(e => computeEquipValue(ns, e, gainRate, profile));
+
+    equips.sort(compareEquips)
+
     for (const equip of equips) {
-        if (info.upgrades.includes(equip) || info.augmentations.includes(equip)) continue;
-        const cost = ns.gang.getEquipmentCost(equip);
-        const stats = ns.gang.getEquipmentStats(equip);
-        const effectiveGain = weightedStatGain(stats, profile);
-        const roi = computeROI(cost, gainRate * effectiveGain);
-        ns.print(`INFO: ROI on buying ${equip} is ${ns.tFormat(roi * 1000)}`);
-        if (roi <= limit) {
-            ns.print(`SUCCESS: buying ${memberName} ${equip}`);
-            ns.gang.purchaseEquipment(memberName, equip);
+        if (info.upgrades.includes(equip.name) || info.augmentations.includes(equip.name)) continue;
+
+        ns.print(`INFO: ROI on buying ${equip.name} is ${ns.tFormat(equip.roi * 1000)}`);
+        if (equip.roi <= limit) {
+            if (ns.gang.purchaseEquipment(memberName, equip.name)) {
+                ns.print(`SUCCESS: buying ${memberName} ${equip.name}`);
+                return;
+            }
         }
+    }
+}
+
+interface EquipValue {
+    name: string;
+    stats: EquipmentStats;
+    cost: number;
+    roi: number;
+}
+
+function computeEquipValue(ns: NS, equip: string, gainRate: number, profile: RoleProfile): EquipValue {
+    const cost = ns.gang.getEquipmentCost(equip);
+    const stats = ns.gang.getEquipmentStats(equip);
+    const effectiveGain = weightedStatGain(stats, profile);
+    const roi = computeROI(cost, gainRate * effectiveGain);
+    return {
+        name: equip,
+        stats,
+        cost,
+        roi,
+    };
+}
+
+function compareEquips(a: EquipValue, b: EquipValue): number {
+    if (!isNaN(a.roi) && isFinite(a.roi) && !isNaN(b.roi) && isFinite(b.roi)) {
+        return a.roi - b.roi;
+    } else {
+        return a.cost - b.cost;
     }
 }
