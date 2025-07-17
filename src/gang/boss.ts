7276fb5928fe8ae909bb44798ce756ed5b8ec597
Zefira Shannon <zefira@hey.com>
2025-07-16 21:07:58 -0700

    [gang] Add more member names

--- src/gang/boss.ts
+++ src/gang/boss.ts
@@ -130,14 +130,41 @@ const NAMES = [
     "Sift",
     "Lylan",
 
+    "Rami",
+    "Xavier",
+
     "Overlord",
     "Uruziel",
+
+    "Fractal",
+    "Lir",
+
+    "Wip",
+    "Qip",
+
+    "Blizz",
+    "Big D",
 
     "Kysandra",
     "Tremine",
 
     "Madda",
     "Padda",
+
+    "Mist",
+    "Thorn",
+
+    "Dusk",
+    "Tarot",
+
+    "Nymthus",
+    "Echo",
+
+    "Chip",
+    "Stump",
+
+    "Ash",
+    "Pine",
 
     "Eiru",
     "Miranda",
