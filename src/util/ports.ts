c068edfb4e11333fededa3d5531c4ae433233e98
Zefira Shannon <zefira@hey.com>
2025-07-19 19:51:36 -0700

    [util] Add documentation to ports API

--- src/util/ports.ts
+++ src/util/ports.ts
@@ -3,6 +3,13 @@ import type { NS, NetscriptPort } from "netscript";
 export const EMPTY_SENTINEL: string = "NULL PORT DATA";
 export const DONE_SENTINEL: string = "PORT CLOSED";
 
+/**
+ * Read all messages available on a port,
+ *
+ * @param ns   - Netscript API object
+ * @param port - NetscriptPort to wait to read from
+ * @yields Messages read from the given port
+ */
 export function* readAllFromPort(ns: NS, port: NetscriptPort) {
     while (true) {
         let nextMsg = port.read();
@@ -13,6 +20,20 @@ export function* readAllFromPort(ns: NS, port: NetscriptPort) {
     }
 }
 
+/**
+ * Run a continuous loop alternately awaiting next write on a
+ * NetscriptPort and reading from the port.
+ *
+ * N.B. the `readFn` should read from the same port passed to
+ * `readLoop`.
+ *
+ * The Promise returned by this function should not be awaited unless
+ * you wish to wait until this script is killed.
+ *
+ * @param ns     - Netscript API object
+ * @param port   - NetscriptPort to wait to read from
+ * @param readFn - Callback to read from and process port messages
+ */
 export async function readLoop(ns: NS, port: NetscriptPort, readFn: () => Promise<void>) {
     const scriptInfo = ns.self();
     let running = true;
