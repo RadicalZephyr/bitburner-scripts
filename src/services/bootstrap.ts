eede54a76e01f0973bb08ce7b4c70bc461b8909c
Zefira Shannon <zefira@hey.com>
2025-07-21 19:03:33 -0700

    [services] Simplify bootstrap script

--- src/services/bootstrap.ts
+++ src/services/bootstrap.ts
@@ -10,70 +10,24 @@ export async function main(ns: NS) {
 
     // We start the Discovery service first because everything else
     // needs the hosts and targets that it finds and cracks.
-    await startDiscover(ns, host);
+    startService(ns, '/services/discover.js', host);
     await ns.sleep(500);
 
-    await startMemory(ns, host);
-    await startPort(ns, host);
+    startService(ns, '/services/memory.js', host);
+    startService(ns, '/services/port.js', host);
 
-    await startUpdater(ns, 'n00dles');
+    startService(ns, '/services/updater.js', 'n00dles');
 
-    await startBackdoor(ns, host);
+    startService(ns, '/services/backdoor-notify.js', host);
 }
 
-async function startMemory(ns: NS, host: string) {
-    const memoryScript = '/services/memory.js';
-
-    const memory = ns.getRunningScript(memoryScript, host);
-    if (memory !== null) {
-        ns.kill(memory.pid);
+function startService(ns: NS, script: string, host: string) {
+    const scriptInfo = ns.getRunningScript(script, host);
+    if (scriptInfo !== null) {
+        ns.kill(scriptInfo.pid);
     }
 
-    manualLaunch(ns, memoryScript, host);
-}
-
-async function startDiscover(ns: NS, host: string) {
-    const discoverScript = '/services/discover.js';
-
-    const discover = ns.getRunningScript(discoverScript, host);
-    if (discover !== null) {
-        ns.kill(discover.pid);
-    }
-
-    manualLaunch(ns, discoverScript, host);
-}
-
-async function startUpdater(ns: NS, host: string) {
-    const updaterScript = '/services/updater.js';
-
-    const updater = ns.getRunningScript(updaterScript, host);
-    if (updater !== null) {
-        ns.kill(updater.pid);
-    }
-
-    manualLaunch(ns, updaterScript, host);
-}
-
-async function startPort(ns: NS, host: string) {
-    const portScript = '/services/port.js';
-
-    const portAllocator = ns.getRunningScript(portScript, host);
-    if (portAllocator !== null) {
-        ns.kill(portAllocator.pid);
-    }
-
-    manualLaunch(ns, portScript, host);
-}
-
-async function startBackdoor(ns: NS, host: string) {
-    const backdoorScript = '/services/backdoor-notify.js';
-
-    const backdoorNotify = ns.getRunningScript(backdoorScript, host);
-    if (backdoorNotify !== null) {
-        ns.kill(backdoorNotify.pid);
-    }
-
-    manualLaunch(ns, backdoorScript, host);
+    manualLaunch(ns, script, host);
 }
 
 function manualLaunch(ns: NS, script: string, hostname: string) {
