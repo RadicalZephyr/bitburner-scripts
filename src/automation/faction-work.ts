697a3aa6096b707afa291ebf5e2a680c10caa587
Zefira Shannon <zefira@hey.com>
2025-07-22 01:43:05 -0700

    [automation] Rebuild faction list during faction work loop
    Big increase in complexity to avoid caching the list of factions the
    player is a member of at the start of the script.

--- src/automation/faction-work.ts
+++ src/automation/faction-work.ts
@@ -3,9 +3,19 @@ import type { FactionWorkType, NS } from 'netscript';
 export async function main(ns: NS) {
     const sing = ns.singularity;
 
-    const factions = ns.getPlayer().factions;
+    const factions = ns
+        .getPlayer()
+        .factions.map((name) => new Faction(ns, name));
+    let unfinishedFactions = factions.filter(
+        (f) => !haveNeededRepForFaction(ns, f),
+    );
 
-    for (const f of factions) {
+    while (unfinishedFactions.length > 0) {
+        unfinishedFactions.sort((a, b) => a.targetRep - b.targetRep);
+
+        const faction = unfinishedFactions.pop();
+        const f = faction.name;
+
         const augs = sing.getAugmentationsFromFaction(f);
         if (augs.length < 1) continue;
 
@@ -41,5 +51,48 @@ export async function main(ns: NS) {
         ) {
             await ns.sleep(1000);
         }
+
+        const factions = ns
+            .getPlayer()
+            .factions.map((name) => new Faction(ns, name));
+        unfinishedFactions = factions.filter(
+            (f) => !haveNeededRepForFaction(ns, f),
+        );
     }
+}
+
+class Faction {
+    name: string;
+    favor: number;
+    targetRep: number;
+
+    constructor(ns: NS, name: string) {
+        this.name = name;
+
+        const sing = ns.singularity;
+
+        this.favor = sing.getFactionFavor(name);
+        const augs = sing.getAugmentationsFromFaction(name);
+
+        if (augs.length < 1) {
+            this.targetRep = 0;
+            return;
+        }
+
+        augs.sort(
+            (a, b) =>
+                sing.getAugmentationRepReq(b) - sing.getAugmentationRepReq(a),
+        );
+
+        this.targetRep = sing.getAugmentationRepReq(augs[0]);
+    }
+}
+
+function haveNeededRepForFaction(ns: NS, faction: Faction) {
+    const sing = ns.singularity;
+    return (
+        faction.targetRep <= sing.getFactionRep(faction.name)
+        || faction.favor + sing.getFactionFavorGain(faction.name)
+            >= ns.getFavorToDonate()
+    );
 }
