67da243eeedafe93c27168d132ba83923366a994
Zefira Shannon <zefira@hey.com>
2025-07-22 15:17:58 -0700

    [automation] Add basic arguments and usage message to buy-augmentations

--- src/automation/buy-augments.ts
+++ src/automation/buy-augments.ts
@@ -1,9 +1,41 @@
 import type { NS } from 'netscript';
 
-import { MEM_TAG_FLAGS } from 'services/client/memory_tag';
+import { ALLOC_ID, MEM_TAG_FLAGS } from 'services/client/memory_tag';
+import { parseAndRegisterAlloc } from 'services/client/memory';
+
+const DEFAULT_SPEND = 1.0;
 
 export async function main(ns: NS) {
-    ns.flags(MEM_TAG_FLAGS);
+    const options = ns.flags([
+        ['spend', DEFAULT_SPEND],
+        ['dry-run', false],
+        ['help', false],
+        ...MEM_TAG_FLAGS,
+    ]);
+
+    if (
+        options.help
+        || typeof options.spend != 'number'
+        || typeof options['dry-run'] != 'boolean'
+    ) {
+        ns.tprint(`
+Usage: ${ns.getScriptName()} [OPTIONS]
+
+Purchase augmentations you have enough rep for from all your factions,
+in order from most expensive to least.
+
+OPTIONS
+  --dry-run  Print out the augmentations you could buy, but don't actually buy anything
+  --spend    Percentage of money to spend on augmentations (default ${ns.formatPercent(DEFAULT_SPEND)})
+  --help     Show this help message
+`);
+        return;
+    }
+
+    const allocationId = await parseAndRegisterAlloc(ns, options);
+    if (options[ALLOC_ID] !== -1 && allocationId === null) {
+        return;
+    }
 
     const sing = ns.singularity;
 
@@ -40,8 +72,16 @@ export async function main(ns: NS) {
     ns.print(JSON.stringify(augs, null, 2));
     ns.ui.renderTail();
 
+    if (options['dry-run']) {
+        ns.ui.openTail();
+        return;
+    }
+
+    const augmentationSpendPercentage = options.spend;
+
     const purchasedAugs = [];
-    let budget = ns.getServerMoneyAvailable('home');
+    let budget =
+        ns.getServerMoneyAvailable('home') * augmentationSpendPercentage;
 
     for (const aug of augs) {
         const cost = sing.getAugmentationPrice(aug.name);
