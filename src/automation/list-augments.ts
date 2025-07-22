d21264f4bfdbaa8604d3167ca0a6ec2f493600e9
Zefira Shannon <zefira@hey.com>
2025-07-22 15:08:36 -0700

    [automation] Rewrite list-augmentations without looping

--- src/automation/list-augments.ts
+++ src/automation/list-augments.ts
@@ -8,42 +8,37 @@ export async function main(ns: NS) {
     const sing = ns.singularity;
 
     ns.disableLog('ALL');
+    const factions = ns.getPlayer().factions;
 
-    while (true) {
-        const factions = ns.getPlayer().factions;
+    const uniqueAugs: Set<string> = new Set();
+    const augs: Aug[] = [];
 
-        const uniqueAugs: Set<string> = new Set();
-        const augs: Aug[] = [];
+    for (const f of factions) {
+        for (const aug of sing.getAugmentationsFromFaction(f)) {
+            if (uniqueAugs.has(aug)) continue;
 
-        for (const f of factions) {
-            for (const aug of sing.getAugmentationsFromFaction(f)) {
-                if (uniqueAugs.has(aug)) continue;
+            uniqueAugs.add(aug);
 
-                uniqueAugs.add(aug);
+            const rep = sing.getAugmentationRepReq(aug);
+            const baseCost = sing.getAugmentationBasePrice(aug);
 
-                const rep = sing.getAugmentationRepReq(aug);
-                const baseCost = sing.getAugmentationBasePrice(aug);
+            const factionRep = sing.getFactionRep(f);
+            if (factionRep < rep) continue;
 
-                const factionRep = sing.getFactionRep(f);
-                if (factionRep < rep) continue;
-
-                augs.push({
-                    name: aug,
-                    faction: f,
-                    rep,
-                    baseCost,
-                });
-            }
+            augs.push({
+                name: aug,
+                faction: f,
+                rep,
+                baseCost,
+            });
         }
-
-        augs.sort((a, b) => b.baseCost - a.baseCost);
-
-        ns.clearLog();
-        ns.print(JSON.stringify(augs, null, 2));
-        ns.ui.renderTail();
-
-        await ns.sleep(1000);
     }
+
+    augs.sort((a, b) => b.baseCost - a.baseCost);
+
+    ns.clearLog();
+    ns.print(JSON.stringify(augs, null, 2));
+    ns.ui.renderTail();
 }
 
 interface Aug {
