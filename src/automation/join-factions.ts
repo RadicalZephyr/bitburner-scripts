32f13580f0b38401259d050fe573c705706e5487
Zefira Shannon <zefira@hey.com>
2025-07-24 18:03:57 -0700

    [automation] Add list of all factions to join-factions script

--- src/automation/join-factions.ts
+++ src/automation/join-factions.ts
@@ -1,7 +1,8 @@
-import type { NS } from 'netscript';
+import type { FactionName, NS } from 'netscript';
 
 export async function main(ns: NS) {
     acceptInvites(ns);
+    pursueInvites(ns);
 
     while (true) {
         await ns.asleep(60_000);
@@ -24,4 +25,83 @@ async function acceptInvites(ns: NS) {
         }
         await ns.asleep(1000);
     }
+}
+
+async function pursueInvites(ns: NS) {
+    const myFactions = new Set(ns.getPlayer().factions);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const factionsToJoin = allFactions(ns).filter((f) => !myFactions.has(f));
+}
+
+function allFactions(ns: NS): FactionName[] {
+    return [
+        ...locationFactions(ns),
+        ...hackingFactions(ns),
+        ...crimeFactions(ns),
+        ...companyFactions(ns),
+        ...miscFactions(ns),
+        ...specialFactions(ns),
+    ];
+}
+
+function locationFactions(ns: NS): FactionName[] {
+    const fn = ns.enums.FactionName;
+    return [
+        fn.Aevum,
+        fn.Chongqing,
+        fn.Ishima,
+        fn.NewTokyo,
+        fn.Sector12,
+        fn.Volhaven,
+    ];
+}
+
+function hackingFactions(ns: NS): FactionName[] {
+    const fn = ns.enums.FactionName;
+    return [
+        fn.CyberSec,
+        fn.NiteSec,
+        fn.TheBlackHand,
+        fn.BitRunners,
+        fn.Daedalus,
+    ];
+}
+
+function crimeFactions(ns: NS): FactionName[] {
+    const fn = ns.enums.FactionName;
+    return [
+        fn.SlumSnakes,
+        fn.Tetrads,
+        fn.SpeakersForTheDead,
+        fn.TheDarkArmy,
+        fn.TheSyndicate,
+        fn.Silhouette,
+        fn.Illuminati,
+    ];
+}
+
+function companyFactions(ns: NS): FactionName[] {
+    const fn = ns.enums.FactionName;
+    return [
+        fn.ECorp,
+        fn.MegaCorp,
+        fn.BachmanAssociates,
+        fn.BladeIndustries,
+        fn.NWO,
+        fn.ClarkeIncorporated,
+        fn.OmniTekIncorporated,
+        fn.FourSigma,
+        fn.KuaiGongInternational,
+        fn.FulcrumSecretTechnologies,
+    ];
+}
+
+function miscFactions(ns: NS): FactionName[] {
+    const fn = ns.enums.FactionName;
+    return [fn.TianDiHui, fn.TheCovenant, fn.Netburners];
+}
+
+function specialFactions(ns: NS): FactionName[] {
+    const fn = ns.enums.FactionName;
+    return [fn.ShadowsOfAnarchy, fn.Bladeburners, fn.ChurchOfTheMachineGod];
 }
