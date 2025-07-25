df7a02e4485cdb401ef20b959d5ddc46f40e9b5b
Zefira Shannon <zefira@hey.com>
2025-07-24 17:41:39 -0700

    [automation] Extract faction invitation accepting into a background task

--- src/automation/join-factions.ts
+++ src/automation/join-factions.ts
@@ -1,15 +1,27 @@
 import type { NS } from 'netscript';
 
 export async function main(ns: NS) {
-    const sing = ns.singularity;
+    acceptInvites(ns);
 
     while (true) {
+        await ns.asleep(60_000);
+    }
+}
+
+async function acceptInvites(ns: NS) {
+    let running = true;
+    ns.atExit(() => {
+        running = false;
+    }, 'acceptInvites');
+
+    const sing = ns.singularity;
+    while (running) {
         const factionInvites = sing.checkFactionInvitations();
 
         for (const f of factionInvites) {
             if (sing.getFactionEnemies(f).length > 0) continue;
             sing.joinFaction(f);
         }
-        await ns.sleep(1000);
+        await ns.asleep(1000);
     }
 }
