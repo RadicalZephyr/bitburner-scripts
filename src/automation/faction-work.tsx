1f20cff34da3503f44fd4904bf0cb31355c1957a
Zefira Shannon <zefira@hey.com>
2025-07-27 00:57:08 -0700

    [automation] Add a focus toggle button to the faction work script

--- src/automation/faction-work.tsx
+++ src/automation/faction-work.tsx
@@ -1,5 +1,10 @@
-import type { FactionWorkType, NS } from 'netscript';
+import type { FactionWorkType, NS, UserInterfaceTheme } from 'netscript';
 import { MEM_TAG_FLAGS } from 'services/client/memory_tag';
+import {
+    KARMA_HEIGHT,
+    STATUS_WINDOW_HEIGHT,
+    STATUS_WINDOW_WIDTH,
+} from '/util/ui';
 
 export async function main(ns: NS) {
     const flags = ns.flags([
@@ -19,7 +24,72 @@ OPTIONS
 `);
     }
 
-    await workForFactions(ns, flags.focus as boolean);
+    ns.disableLog('ALL');
+    ns.clearLog();
+
+    ns.ui.openTail();
+    ns.ui.resizeTail(STATUS_WINDOW_WIDTH, KARMA_HEIGHT);
+    const [ww] = ns.ui.windowSize();
+    ns.ui.moveTail(
+        ww - STATUS_WINDOW_WIDTH,
+        STATUS_WINDOW_HEIGHT + KARMA_HEIGHT,
+    );
+
+    const focus = new Toggle(ns, flags.focus as boolean);
+    ns.printRaw(<FocusToggle ns={ns} focus={focus} />);
+    ns.ui.renderTail();
+
+    await workForFactions(ns, focus);
+    ns.tprint('finished faction work');
+}
+
+class Toggle {
+    ns: NS;
+    value: boolean;
+
+    constructor(ns: NS, value: boolean) {
+        this.ns = ns;
+        this.value = value;
+    }
+
+    toggle() {
+        this.value = !this.value;
+        this.ns.singularity.setFocus(this.value);
+    }
+}
+
+interface FocusProps {
+    ns: NS;
+    focus: Toggle;
+}
+
+const buttonClass =
+    'MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium css-u8jh2y';
+
+function FocusToggle({ ns, focus }: FocusProps) {
+    const [theme, setTheme] = React.useState(
+        ns.ui.getTheme() as UserInterfaceTheme,
+    );
+
+    React.useEffect(() => {
+        const id = globalThis.setInterval(() => {
+            setTheme(ns.ui.getTheme());
+        }, 200);
+
+        return () => {
+            globalThis.clearInterval(id);
+        };
+    }, [ns]);
+
+    return (
+        <button
+            className={buttonClass}
+            style={{ color: theme.successlight }}
+            onClick={() => focus.toggle()}
+        >
+            Toggle Focus
+        </button>
+    );
 }
 
 class Faction {
@@ -55,7 +125,7 @@ class Faction {
     }
 }
 
-async function workForFactions(ns: NS, focus: boolean) {
+async function workForFactions(ns: NS, focus: Toggle) {
     const sing = ns.singularity;
 
     while (true) {
@@ -70,14 +140,16 @@ async function workForFactions(ns: NS, focus: boolean) {
 
         const workType = getBestWorkTypeForFaction(ns, lowestRepFaction.name);
 
-        if (!sing.workForFaction(lowestRepFaction.name, workType, focus)) {
+        if (
+            !sing.workForFaction(lowestRepFaction.name, workType, focus.value)
+        ) {
             ns.print(
                 `WARN: could not start working ${workType} for ${lowestRepFaction.name}`,
             );
             return;
         }
 
-        await ns.sleep(10_000);
+        await ns.asleep(10_000);
     }
 }
 
