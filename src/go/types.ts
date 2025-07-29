9efccf76b428863165bf7e5ca867bbcc7b053130
Zefira Shannon <zefira@hey.com>
2025-07-29 00:29:33 -0700

    [go] Add stronger types for Go board concepts

---
+++ src/go/types.ts
@@ -0,0 +1,53 @@
+export const COL_NAMES = [
+    'a' as const,
+    'b' as const,
+    'c' as const,
+    'd' as const,
+    'e' as const,
+    'f' as const,
+    'g' as const,
+    'h' as const,
+    'j' as const,
+    'k' as const,
+    'l' as const,
+    'm' as const,
+    'n' as const,
+    'o' as const,
+    'p' as const,
+    'q' as const,
+    'r' as const,
+    's' as const,
+    't' as const,
+];
+
+export type Col = (typeof COL_NAMES)[number];
+
+export const ROW_NAMES = [
+    1 as const,
+    2 as const,
+    3 as const,
+    4 as const,
+    5 as const,
+    6 as const,
+    7 as const,
+    8 as const,
+    9 as const,
+    10 as const,
+    11 as const,
+    12 as const,
+    13 as const,
+    14 as const,
+    15 as const,
+    16 as const,
+    17 as const,
+    18 as const,
+    19 as const,
+];
+
+export type Row = (typeof ROW_NAMES)[number];
+
+export type Vertex = `${Col}${Row}`;
+
+export type Color = 'white' | 'w' | 'W' | 'black' | 'b' | 'B';
+
+export type Move = [Color, Vertex];
