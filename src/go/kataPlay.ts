c2f510bf199a53cd69cf388d5f6998e5bad78e0a
Zefira Shannon <zefira@hey.com>
2025-07-29 00:58:24 -0700

    [go] Use new filterMapBoard function in disabledNodes function

--- src/go/kataPlay.ts
+++ src/go/kataPlay.ts
@@ -1,28 +1,18 @@
 import type { NS } from 'netscript';
 
 import { GtpClient } from 'go/GtpClient';
-import { COL_NAMES, ROW_NAMES, Vertex } from 'go/types';
+import { filterMapBoard, Vertex } from 'go/types';
 
-const INVALID_NODE = '#';
+const DISABLED_NODE = '#';
 
 export async function main(ns: NS) {
     const client = new GtpClient(ns);
 }
 
 function disabledNodes(board: string[]): Vertex[] {
-    const nodes = [];
-    for (let i = 0; i < board.length; i++) {
-        const col = COL_NAMES[i];
-        const columnArray = board[i].split('');
-        for (let j = 0; j < board.length; j++) {
-            const node = columnArray[j];
-            const row = ROW_NAMES[j];
-            if (node === INVALID_NODE) {
-                nodes.push(`${col}${row}` as Vertex);
-            }
-        }
-    }
-    return nodes;
+    return filterMapBoard(board, (node, vertex) =>
+        node === DISABLED_NODE ? vertex : undefined,
+    );
 }
 
 async function setupExistingGame(ns: NS, client: GtpClient) {
