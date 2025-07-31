f977dcbe4141e654ec18f68a9b7dc3da5a23a49d
Zefira Shannon <zefira@hey.com>
2025-07-31 11:41:39 -0700

    [automation] WIP: add sketch of early bitnode automation script

---
+++ src/automation/early-bitnode.ts
@@ -0,0 +1,7 @@
+import type { NS } from 'netscript';
+
+export async function main(ns: NS) {
+    // Study algorithms at Rothman U
+    // Wait until we can create BruteSSH.exe
+    // Create BruteSSH.exe
+}
