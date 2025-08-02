1ef42573bcbeb900df31a627fbb51992d5c8a669
Zefira Shannon <zefira@hey.com>
2025-08-01 23:55:06 -0700

    [go] Encode set_positions and set_free_handicap more simply
    Instead of sending the data as a json array which adds a bunch of
    extra characters (brackets, quotes and commas), just join the data
    with spaces and url encode it.

--- src/go/GtpClient.ts
+++ src/go/GtpClient.ts
@@ -70,7 +70,7 @@ export class GtpClient {
         const flatPositions = positions.reduce((acc, n) => extend(acc, n), []);
         await this.send(
             'set_position',
-            encodeURIComponent(JSON.stringify(flatPositions)),
+            encodeURIComponent(flatPositions.join(' ')),
         );
     }
 
@@ -82,7 +82,7 @@ export class GtpClient {
     async setFreeHandicap(vertices: Vertex[]) {
         await this.send(
             'set_free_handicap',
-            encodeURIComponent(JSON.stringify(vertices)),
+            encodeURIComponent(vertices.join(' ')),
         );
     }
 
