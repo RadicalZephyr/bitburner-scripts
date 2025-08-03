49383c5659b31f0a88fc2c5db1d0124dff3cb1f4
Zefira Shannon <zefira@hey.com>
2025-08-02 22:53:08 -0700

    [automation] Add a script to grow all servers manually

---
+++ src/automation/grow-all.ts
@@ -0,0 +1,39 @@
+import type { NS } from 'netscript';
+
+import { shortestPath } from 'util/shortest-path';
+import { sendTerminalCommand } from 'util/terminal';
+import { walkNetworkBFS } from 'util/walk';
+
+export async function main(ns: NS) {
+    const network = walkNetworkBFS(ns);
+    const hosts = network.keys();
+
+    for (const host of hosts) {
+        const maxMoney = ns.getServerMaxMoney(host);
+        const curMoney = ns.getServerMoneyAvailable(host);
+        if (curMoney >= maxMoney * 0.99) continue;
+
+        await manualGrowHost(ns, host);
+    }
+}
+
+async function manualGrowHost(ns: NS, host: string) {
+    const currentHost = ns.singularity.getCurrentServer();
+    const path = await shortestPath(ns, currentHost, host);
+    await traverseNetworkPath(ns, path);
+    await manualGrow(ns);
+}
+
+async function traverseNetworkPath(ns: NS, path: string[]) {
+    for (const host of path) {
+        const currentHost = ns.singularity.getCurrentServer();
+        if (!ns.singularity.connect(host))
+            throw new Error(`failed to connect to ${host} from ${currentHost}`);
+    }
+    await ns.sleep(0);
+}
+
+async function manualGrow(ns: NS) {
+    sendTerminalCommand('grow');
+    await ns.sleep(2000);
+}
