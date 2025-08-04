e38cd504e689a5164fad3930ddb48039c1f17d6b
Zefira Shannon <zefira@hey.com>
2025-08-03 21:15:59 -0700

    [services] Move simple allocation test above test group

--- src/services/__tests__/new_allocator.test.ts
+++ src/services/__tests__/new_allocator.test.ts
@@ -66,6 +66,23 @@ describe('basic Worker CRUD', () => {
         expect(w2.freeRam).toBeCloseTo(15.97);
     });
 
+    test('workers track RAM allocations', () => {
+        const w1 = new Worker('a', 8);
+        expect(w1.usedRam).toBe(0);
+
+        expect(w1.allocate(2, 1)).toEqual({
+            hostname: 'a',
+            chunkSize: 2,
+            numChunks: 1,
+        });
+        expect(w1.usedRam).toBe(2);
+        expect(w1.freeRam).toBe(6);
+
+        w1.free(2, 1);
+        expect(w1.usedRam).toBe(0);
+        expect(w1.freeRam).toBe(8);
+    });
+
     describe('worker cannot allocate more than total RAM', () => {
         test('in one shot', () => {
             const w1 = new Worker('a', 4);
@@ -118,22 +135,5 @@ describe('basic Worker CRUD', () => {
             expect(w1.allocate(1, 1)).not.toBeNull();
             expect(w1.allocate(1.01, 1)).toBeNull();
         });
-    });
-
-    test('workers track RAM allocations', () => {
-        const w1 = new Worker('a', 8);
-        expect(w1.usedRam).toBe(0);
-
-        expect(w1.allocate(2, 1)).toEqual({
-            hostname: 'a',
-            chunkSize: 2,
-            numChunks: 1,
-        });
-        expect(w1.usedRam).toBe(2);
-        expect(w1.freeRam).toBe(6);
-
-        w1.free(2, 1);
-        expect(w1.usedRam).toBe(0);
-        expect(w1.freeRam).toBe(8);
     });
 });
