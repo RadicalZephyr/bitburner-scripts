392af9fcfd0878055a25ebb8f0ed06fd4f199b14
Zefira Shannon <zefira@hey.com>
2025-08-14 03:47:05 -0700

    [graft] Add command line flags for specifying multipliers to target

--- src/graft/q.ts
+++ src/graft/q.ts
@@ -3,13 +3,57 @@ import { FlagsSchema, parseFlags } from 'util/flags';
 
 import { canAfford } from 'util/money';
 
+const MULTIPLIERS = [
+    'hacking',
+    'hacking_exp',
+    'strength',
+    'strength_exp',
+    'defense',
+    'defense_exp',
+    'dexterity',
+    'dexterity_exp',
+    'agility',
+    'agility_exp',
+    'charisma',
+    'charisma_exp',
+    'hacking_chance',
+    'hacking_speed',
+    'hacking_money',
+    'hacking_grow',
+    'company_rep',
+    'faction_rep',
+    'crime_money',
+    'crime_success',
+    'work_money',
+    'hacknet_node_money',
+    'hacknet_node_purchase_cost',
+    'hacknet_node_ram_cost',
+    'hacknet_node_core_cost',
+    'hacknet_node_level_cost',
+    'bladeburner_max_stamina',
+    'bladeburner_stamina_gain',
+    'bladeburner_analysis',
+    'bladeburner_success_chance',
+] as const satisfies readonly (keyof Multipliers)[];
+
 const FLAGS = [
     ['dry-run', false],
+    ['mult', ['hacking_speed', 'hacking_chance']],
     ['help', false],
 ] as const satisfies FlagsSchema;
 
-export function autocomplete(data: AutocompleteData): string[] {
+export function autocomplete(data: AutocompleteData, args: string[]): string[] {
     data.flags(FLAGS);
+
+    const multFlag = '--mult';
+
+    const last = args.at(-1);
+    if (last === multFlag) return MULTIPLIERS;
+
+    const secondLast = args.at(-2);
+    if (secondLast === multFlag)
+        return MULTIPLIERS.filter((m) => m.startsWith(last));
+
     return [];
 }
 
@@ -20,28 +64,43 @@ export async function main(ns: NS) {
         ns.tprint(`
 USAGE: run ${ns.getScriptName()}
 
-Graft augments in order of least time and selecting for specific skill increases.
+Graft augments in order of least time and selecting for specific multipliers.
 
 Example:
-> run ${ns.getScriptName()}
+  > run ${ns.getScriptName()} --mult faction_rep
+  > run ${ns.getScriptName()} --mult hacking --mult hacking_exp
 
 OPTIONS
   --dry-run  Don't buy anything, just display the augments that would be chosen
+  --mult     Augmentation multipliers to filter by, may be specified multiple times
+             Default: hacking_speed, hacking_chance
+             Available multipliers: ${MULTIPLIERS.join(', ')}
   --help     Show this help message
 `);
         return;
     }
 
-    await graftAugments(ns, flags['dry-run']);
+    await graftAugments(ns, flags['dry-run'], flags.mult);
 
     ns.ui.openTail();
 }
 
-async function graftAugments(ns: NS, dryRun: boolean) {
+async function graftAugments(ns: NS, dryRun: boolean, mult: string[]) {
+    const mults = mult
+        .map((m) => m.trim())
+        .filter((m): m is keyof Multipliers => m.length > 0);
+
+    for (const m of mults) {
+        if (!MULTIPLIERS.includes(m)) {
+            ns.tprint(`ERROR: Unknown multiplier '${m}'.`);
+            return;
+        }
+    }
+
     const graftableAugs = ns.grafting
         .getGraftableAugmentations()
         .map((a) => augment(ns, a))
-        .filter((a) => 'hacking_speed' in a || 'hacking_chance' in a); // TODO: make which multipliers to filter by configurable
+        .filter((a) => mults.some((m) => m in a));
 
     graftableAugs.sort((a, b) => {
         if (Math.abs(a.price - b.price) < 1) {
