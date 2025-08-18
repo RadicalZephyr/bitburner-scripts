fcd5814c4b5ae913b73e6f3ae028cdefaee320e7
Zefira Shannon <zefira@hey.com>
2025-08-17 18:07:23 -0700

    [services] Include mega-corporation servers in backdoor list

--- src/services/backdoor-notify.tsx
+++ src/services/backdoor-notify.tsx
@@ -11,7 +11,17 @@ const FACTION_SERVERS = [
     'avmnite-02h',
     'I.I.I.I',
     'run4theh111z',
-    'The-Cave',
+    'b-and-a',
+    'blade',
+    'clarkinc',
+    'ecorp',
+    '4sigma',
+    'fulcrumassets',
+    'fulcrumtech',
+    'kuai-gong',
+    'megacorp',
+    'nwo',
+    'omnitek',
 ];
 
 export async function main(ns: NS) {
