d38dd21fb3200c4bf562ae0d9f58766ed16862e0
Zefira Shannon <zefira@hey.com>
2025-08-19 03:23:15 -0700

    [sleeve] Add super basic Bladeburner script

---
+++ src/sleeve/bladeburner.ts
@@ -0,0 +1,42 @@
+import type { NS, AutocompleteData } from 'netscript';
+import { FlagsSchema, parseFlags } from 'util/flags';
+
+const FLAGS = [['help', false]] as const satisfies FlagsSchema;
+
+export function autocomplete(data: AutocompleteData): string[] {
+    data.flags(FLAGS);
+    return [];
+}
+
+export async function main(ns: NS) {
+    const flags = await parseFlags(ns, FLAGS);
+
+    if (flags.help) {
+        ns.tprint(`
+USAGE: run ${ns.getScriptName()}
+
+Set sleeves to working on Bladeburner.
+
+Example:
+  > run ${ns.getScriptName()}
+
+OPTIONS
+  --help   Show this help message
+`);
+        return;
+    }
+
+    await doBladeburnerStuff(ns);
+}
+
+async function doBladeburnerStuff(ns: NS) {
+    const numSleeves = ns.sleeve.getNumSleeves();
+    let i = 0;
+    for (; i < Math.floor(numSleeves / 2); i++) {
+        ns.sleeve.setToBladeburnerAction(i, 'Field Analysis');
+    }
+
+    for (; i < numSleeves; i++) {
+        ns.sleeve.setToBladeburnerAction(i, 'Training');
+    }
+}
