49d432a17b80a482480fc6e82e8d8748dd3c3698
Zefira Shannon <zefira@hey.com>
2025-08-23 23:14:17 -0700

    [automation] Add scripts to automate tilling and sowing from the terminal

---
+++ src/automation/manual-till.ts
@@ -0,0 +1,46 @@
+import type { NS, AutocompleteData } from 'netscript';
+import { FlagsSchema, parseFlags } from 'util/flags';
+
+import { sendTerminalCommand } from 'util/terminal';
+
+const FLAGS = [['help', false]] as const satisfies FlagsSchema;
+
+export function autocomplete(data: AutocompleteData): string[] {
+    data.flags(FLAGS);
+    return data.servers;
+}
+
+export async function main(ns: NS) {
+    const flags = await parseFlags(ns, FLAGS);
+
+    if (
+        flags.help
+        || flags._.length !== 1
+        || typeof flags._[0] !== 'string'
+        || !ns.serverExists(flags._[0])
+    ) {
+        ns.tprint(`
+USAGE: run ${ns.getScriptName()} TARGET
+
+Manually till a target server using terminal commands.
+
+Example:
+  > run ${ns.getScriptName()} foodnstuff
+
+OPTIONS
+  --help   Show this help message
+`);
+        return;
+    }
+
+    const target = flags._[0];
+    await manuallySow(ns, target);
+}
+
+async function manuallySow(ns: NS, target: string) {
+    await sendTerminalCommand(ns, `connect ${target}`);
+
+    while (true) {
+        await sendTerminalCommand(ns, 'weaken ; weaken ; weaken');
+    }
+}
