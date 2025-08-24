73d0b00b2f8bea49eb6e1e5be99b58922084d7f6
Zefira Shannon <zefira@hey.com>
2025-08-24 00:04:12 -0700

    [automation] Add a script to donate to all factions
    It determines the amount of reputation needed to purchase the augment
    with with the highest rep requirement and then calculates the amount
    to donate to receive that much reputation.

---
+++ src/automation/donate.ts
@@ -0,0 +1,64 @@
+import type { NS, AutocompleteData } from 'netscript';
+import { FlagsSchema, parseFlags } from 'util/flags';
+
+const FLAGS = [['help', false]] as const satisfies FlagsSchema;
+
+export function autocomplete(data: AutocompleteData): string[] {
+    data.flags(FLAGS);
+    return [];
+}
+
+export async function main(ns: NS) {
+    const flags = await parseFlags(ns, FLAGS);
+
+    if (flags.help) {
+        ns.tprint(`
+USAGE: run ${ns.getScriptName()}
+
+Donate to all factions to make all augments available for sleeves to buy.
+
+Example:
+  > run ${ns.getScriptName()}
+
+OPTIONS
+  --help   Show this help message
+`);
+        return;
+    }
+
+    await donateForAugments(ns);
+}
+
+async function donateForAugments(ns: NS) {
+    const player = ns.getPlayer();
+    const factions = player.factions;
+
+    for (const f of factions) {
+        const favor = ns.singularity.getFactionFavor(f);
+
+        // Skip factions we can't donate to
+        if (favor < ns.getFavorToDonate()) continue;
+
+        const rep = ns.singularity.getFactionRep(f);
+        const maxRep = getMaxRep(ns, f);
+        const repDelta = maxRep - rep;
+        const donation = ns.formulas.reputation.donationForRep(
+            repDelta,
+            player,
+        );
+        const result = ns.singularity.donateToFaction(f, donation);
+        if (!result) ns.print(`WARN: failed to donate to ${f}`);
+    }
+}
+
+function getMaxRep(ns: NS, f: string): number {
+    const augs = ns.singularity.getAugmentationsFromFaction(f).map((a) => {
+        return { name: a, rep: ns.singularity.getAugmentationRepReq(a) };
+    });
+
+    if (augs.length === 0) return 0;
+
+    augs.sort((a, b) => b.rep - a.rep);
+
+    return augs[0].rep;
+}
