a5bf996d7632cd33310447619532f77b7b0c92b6
Zefira Shannon <zefira@hey.com>
2025-08-26 01:32:07 -0700

    [dir-locals] Add dir local config to use our version of typescript

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((tsx-ts-mode . ((tide-tsserver-executable . "node_modules/typescript/bin/tsserver")))
+ (typescript-ts-mode . ((tide-tsserver-executable . "node_modules/typescript/bin/tsserver"))))
