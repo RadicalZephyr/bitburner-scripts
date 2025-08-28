24a83975e0f217ab7fa08d88ea2c054755080e15
Zefira Shannon <zefira@hey.com>
2025-08-27 22:24:09 -0700

    [sleeve] Allow setting a specific course in study script

--- src/sleeve/study.ts
+++ src/sleeve/study.ts
@@ -1,17 +1,37 @@
-import type { NS, AutocompleteData } from 'netscript';
+import type {
+    NS,
+    AutocompleteData,
+    UniversityClassType,
+    NSEnums,
+} from 'netscript';
 import { FlagsSchema, parseFlags } from 'util/flags';
 
-const FLAGS = [['help', false]] as const satisfies FlagsSchema;
+const FLAGS = [
+    ['course', 'Algorithms'],
+    ['help', false],
+] as const satisfies FlagsSchema;
 
-export function autocomplete(data: AutocompleteData): string[] {
+export function autocomplete(data: AutocompleteData, args: string[]): string[] {
     data.flags(FLAGS);
+
+    const secondLast = args.at(-2);
+    const last = args.at(-1);
+
+    const courseFlag = '--course';
+    const courses = courseTypes(data.enums).map((course) => `'${course}'`);
+
+    if (last === courseFlag) return courses;
+
+    if (secondLast === courseFlag)
+        return courses.filter((c) => c.startsWith(last));
+
     return [];
 }
 
 export async function main(ns: NS) {
     const flags = await parseFlags(ns, FLAGS);
 
-    if (flags.help) {
+    if (flags.help || !isCourse(ns, flags.course)) {
         ns.tprint(`
 USAGE: run ${ns.getScriptName()}
 
@@ -21,15 +41,19 @@ Example:
   > run ${ns.getScriptName()}
 
 OPTIONS
+  --course Which course of study to start
   --help   Show this help message
 `);
         return;
     }
 
-    studyAtUniversity(ns);
+    studyAtUniversity(ns, flags.course);
 }
 
-function studyAtUniversity(ns: NS) {
+function studyAtUniversity(
+    ns: NS,
+    course: UniversityClassType | `${UniversityClassType}`,
+) {
     const numSleeves = ns.sleeve.getNumSleeves();
     for (let i = 0; i < numSleeves; i++) {
         if (!ns.sleeve.travel(i, 'Volhaven'))
@@ -38,9 +62,25 @@ function studyAtUniversity(ns: NS) {
             !ns.sleeve.setToUniversityCourse(
                 i,
                 'ZB Institute of Technology',
-                'Algorithms',
+                course,
             )
         )
-            throw new Error('failed to study Algorithms at ZBU');
+            throw new Error(`failed to study ${course} at ZBU`);
     }
+}
+
+function isCourse(ns: NS, course: string): course is UniversityClassType {
+    const courses = new Set(courseTypes(ns.enums)) as Set<string>;
+    return courses.has(course);
+}
+
+function courseTypes(enums: NSEnums): UniversityClassType[] {
+    return [
+        enums.UniversityClassType.computerScience,
+        enums.UniversityClassType.dataStructures,
+        enums.UniversityClassType.networks,
+        enums.UniversityClassType.algorithms,
+        enums.UniversityClassType.management,
+        enums.UniversityClassType.leadership,
+    ];
 }
