75b2ccf0b86f6ede7e3846ad2b0b469f38d8632a
Zefira Shannon <zefira@hey.com>
2025-08-27 22:58:30 -0700

    [automation] Incorporate crime success chance into money per second calculation

--- src/automation/best-crime.ts
+++ src/automation/best-crime.ts
@@ -1,4 +1,10 @@
-import type { NS, AutocompleteData, CrimeType, CrimeStats } from 'netscript';
+import type {
+    NS,
+    AutocompleteData,
+    CrimeType,
+    CrimeStats,
+    Player,
+} from 'netscript';
 import { FlagsSchema, parseFlags } from 'util/flags';
 
 const FLAGS = [['help', false]] as const satisfies FlagsSchema;
@@ -34,7 +40,12 @@ async function printBestCrime(ns: NS) {
         .map((c) => crimeStats(ns, ns.enums.CrimeType[c]))
         .map(
             // c.time is in milliseconds, convert to seconds
-            (c) => `${c.name}: $${ns.formatNumber(c.money / (c.time / 1000))}`,
+            (c) => {
+                const successChance = crimeSuccessChance(ns, c);
+                const expectedMoneyPerSec =
+                    (c.money * successChance) / (c.time / 1000);
+                return `${c.name}: $${ns.formatNumber(expectedMoneyPerSec)}`;
+            },
         );
 
     ns.tprint(`Crime money/s:\n${crimes.join('\n')}`);
@@ -46,4 +57,37 @@ interface Crime extends CrimeStats {
 
 function crimeStats(ns: NS, name: CrimeType): Crime {
     return { name, ...ns.singularity.getCrimeStats(name) };
+}
+
+function crimeSuccessChance(ns: NS, crime: Crime): number {
+    if (ns.fileExists('Formulas.exe', 'home')) {
+        return ns.formulas.work.crimeSuccessChance(ns.getPlayer(), crime.name);
+    } else {
+        return estimateCrimeSuccessChance(ns.getPlayer(), crime);
+    }
+}
+
+const CONSTANTS = {
+    IntelligenceCrimeWeight: 0.025,
+    MaxSkillLevel: 975,
+};
+
+function estimateCrimeSuccessChance(p: Player, crime: Crime): number {
+    {
+        let chance: number =
+            crime.hacking_success_weight * p.skills.hacking
+            + crime.strength_success_weight * p.skills.strength
+            + crime.defense_success_weight * p.skills.defense
+            + crime.dexterity_success_weight * p.skills.dexterity
+            + crime.agility_success_weight * p.skills.agility
+            + crime.charisma_success_weight * p.skills.charisma
+            + CONSTANTS.IntelligenceCrimeWeight * p.skills.intelligence;
+        chance /= CONSTANTS.MaxSkillLevel;
+        chance /= crime.difficulty;
+        chance *= p.mults.crime_success;
+        // chance *= currentNodeMults.CrimeSuccessRate;
+        // chance *= calculateIntelligenceBonus(p.skills.intelligence, 1);
+
+        return Math.min(chance, 1);
+    }
 }
