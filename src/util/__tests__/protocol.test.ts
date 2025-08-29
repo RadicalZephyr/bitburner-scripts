1d7634c972f9776849d74b455241b0c13e069f45
Zefira Shannon <zefira@hey.com>
2025-08-28 23:40:00 -0700

    [util] Add tests demonstrating the basic shape of a ProtocolDef

--- src/util/__tests__/protocol.test.ts
+++ src/util/__tests__/protocol.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 
-import { type Validator } from '../protocol';
+import { type ProtocolDef, type Validator } from '../protocol';
 
 describe('our protocol abstraction', () => {
     test('is based on Validator functions', () => {
@@ -15,5 +15,34 @@ describe('our protocol abstraction', () => {
         expect(isValid(1)).toBeFalsy();
         expect(isValid(1n)).toBeFalsy();
         expect(isValid('thing')).toBeFalsy();
+    });
+});
+
+describe('protocol definitions map a message type', () => {
+    test('to a payload', () => {
+        const testProtoType = 'foo';
+        const TestProtoDef = {
+            [testProtoType]: {
+                payload: (v: unknown): v is number => typeof v === 'number',
+            },
+        } as const satisfies ProtocolDef;
+
+        const validate = TestProtoDef[testProtoType].payload;
+        expect(validate(1)).toBeTruthy();
+        expect(validate('')).toBeFalsy();
+    });
+
+    test('and an optional response', () => {
+        const testProtoType = 'foo';
+        const TestProtoDef = {
+            [testProtoType]: {
+                payload: (v: unknown): v is number => typeof v === 'number',
+                response: (v: unknown): v is string => typeof v === 'string',
+            },
+        } as const satisfies ProtocolDef; // todo
+
+        const validate = TestProtoDef[testProtoType].response;
+        expect(validate('')).toBeTruthy();
+        expect(validate(1)).toBeFalsy();
     });
 });
--- src/util/protocol.ts
+++ src/util/protocol.ts
@@ -1,1 +1,12 @@
 export type Validator<T> = (v: unknown) => v is T;
+
+export type ProtocolDef = Record<
+    string,
+    {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        payload: Validator<any>;
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        response?: Validator<any>; // optional, cannot receive replies if missing
+    }
+>;
