43ab08ee2da9cde45ed67efbca3e3c4458654b9c
Zefira Shannon <zefira@hey.com>
2025-09-05 09:41:20 -0700

    [util] Remove unused ports readLoop function

--- src/util/ports.ts
+++ src/util/ports.ts
@@ -1,7 +1,5 @@
 import type { NS, NetscriptPort } from 'netscript';
 
-import { makeFuid } from 'util/fuid';
-
 export const EMPTY_SENTINEL: string = 'NULL PORT DATA';
 export const DONE_SENTINEL: string = 'PORT CLOSED';
 
@@ -22,47 +20,5 @@ export function* readAllFromPort(ns: NS, port: NetscriptPort) {
             return;
         }
         yield nextMsg;
-    }
-}
-
-/**
- * Run a continuous loop alternately awaiting next write on a
- * NetscriptPort and reading from the port.
- *
- * N.B. the `readFn` should read from the same port passed to
- * `readLoop`.
- *
- * The Promise returned by this function should not be awaited unless
- * you wish to wait until this script is killed.
- *
- * Any exceptions thrown by `readFn` are logged with a warning and the
- * loop continues.
- *
- * @param ns     - Netscript API object
- * @param port   - NetscriptPort to wait to read from
- * @param readFn - Callback to read from and process port messages
- */
-export async function readLoop(
-    ns: NS,
-    port: NetscriptPort,
-    readFn: () => Promise<void>,
-) {
-    let running = true;
-    ns.atExit(
-        () => {
-            running = false;
-        },
-        `readLoop-${makeFuid(ns)}`,
-    );
-
-    let next = port.nextWrite();
-    while (running) {
-        try {
-            await readFn();
-        } catch (err) {
-            ns.print(`WARN: failed to read from port ${String(err)}`);
-        }
-        await next;
-        next = port.nextWrite();
     }
 }
