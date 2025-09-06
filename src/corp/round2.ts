9a98b2298c8fefba499d78495dec1be21a09c5a6
Zefira Shannon <zefira@hey.com>
2025-09-06 02:50:22 -0700

    [corp] Add expansion to chemical division to round2 script

--- src/corp/round2.ts
+++ src/corp/round2.ts
@@ -1,12 +1,15 @@
 import type {
     AutocompleteData,
     CityName,
-    CorporationInfo,
+    CorpEmployeePosition,
     Division,
     NS,
 } from 'netscript';
 import { FlagsSchema, parseFlags } from 'util/flags';
+
 import { DispatchClient, DispatchFn } from 'services/client/dispatch';
+
+import { CHEM_DIVISION, CITIES } from 'corp/constants';
 
 const FLAGS = [['help', false]] as const satisfies FlagsSchema;
 
@@ -39,6 +42,8 @@ OPTIONS
     }
 
     await upgradeRound2(ns);
+
+    await expandToChem(ns);
 }
 
 async function upgradeRound2(ns: NS) {
@@ -64,9 +69,71 @@ async function upgradeOffice(
     division: Division,
     city: CityName,
 ) {
-    const office = await _ns('corporation.getOffice', division.name, city);
+    await expandOfficeSizeTo(_ns, division, city, 8);
 
-    const newOfficeSize = 8;
+    await setAllWorkersTo(_ns, division, city, 'Research & Development');
+}
+
+async function expandToChem(ns: NS) {
+    const dispatchClient = new DispatchClient(ns);
+    const _ns = dispatchClient.dispatch.bind(dispatchClient) as DispatchFn;
+
+    const corpInfo = await _ns('corporation.getCorporation');
+    const divisions = await Promise.all(
+        corpInfo.divisions.map((name) => _ns('corporation.getDivision', name)),
+    );
+
+    let division = divisions.find((d) => d.type === 'Chemical');
+    if (!division) {
+        await _ns('corporation.expandIndustry', 'Chemical', CHEM_DIVISION);
+        division = await _ns('corporation.getDivision', CHEM_DIVISION);
+        if (!division) {
+            ns.ui.openTail();
+            ns.print(`WARN: Could not expand into Agriculture`);
+            return;
+        }
+    }
+
+    const divCities = new Set(division.cities);
+    for (const city of CITIES) {
+        if (!divCities.has(city)) {
+            await _ns('corporation.expandCity', division.name, city);
+        }
+
+        let warehouse = await _ns(
+            'corporation.getWarehouse',
+            division.name,
+            city,
+        );
+        if (!warehouse) {
+            await _ns('corporation.purchaseWarehouse', division.name, city);
+            warehouse = await _ns(
+                'corporation.getWarehouse',
+                division.name,
+                city,
+            );
+            if (!warehouse) {
+                ns.ui.openTail();
+                ns.print(
+                    `WARN: could not buy warehouse for ${division.name} in ${city}`,
+                );
+                return;
+            }
+        }
+
+        await expandOfficeSizeTo(_ns, division, city, 4);
+
+        await setAllWorkersTo(_ns, division, city, 'Research & Development');
+    }
+}
+
+async function expandOfficeSizeTo(
+    _ns: DispatchFn,
+    division: Division,
+    city: CityName,
+    newOfficeSize: number,
+) {
+    const office = await _ns('corporation.getOffice', division.name, city);
     if (office.size < newOfficeSize) {
         const seatsToAdd = Math.max(0, newOfficeSize - office.size);
         await _ns(
@@ -79,7 +146,15 @@ async function upgradeOffice(
     for (let i = office.numEmployees; i < newOfficeSize; i++) {
         await _ns('corporation.hireEmployee', division.name, city);
     }
+}
 
+async function setAllWorkersTo(
+    _ns: DispatchFn,
+    division: Division,
+    city: CityName,
+    task: CorpEmployeePosition,
+) {
+    const office = await _ns('corporation.getOffice', division.name, city);
     for (const job in office.employeeJobs) {
         if (office.employeeJobs[job] === 0) continue;
         await _ns(
@@ -94,7 +169,7 @@ async function upgradeOffice(
         'corporation.setAutoJobAssignment',
         division.name,
         city,
-        'Research & Development',
-        newOfficeSize,
+        task,
+        office.numEmployees,
     );
 }
