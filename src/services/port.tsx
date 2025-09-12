7207cb157fe8a632e17fbc491b8a7b542de51f9c
Zefira Shannon <zefira@hey.com>
2025-09-12 10:59:58 -0700

    [services] Add a custom UI to the port allocator

--- src/services/port.tsx
+++ src/services/port.tsx
@@ -1,4 +1,4 @@
-import type { NS } from 'netscript';
+import type { NS, UserInterfaceTheme } from 'netscript';
 import { parseFlags } from 'util/flags';
 
 import {
@@ -10,7 +10,14 @@ import {
 } from 'services/client/port';
 import { MemoryClient } from 'services/client/memory';
 
+import {} from 'lib/react';
+
+import { LogRoot } from 'ui/LogRoot';
+
+import { usePoll, useNsUpdate, useTheme } from 'util/hooks';
+import { installLogger } from 'util/logger';
 import { BaseServer, type Handlers } from 'util/protocol';
+import { RingBuffer } from 'util/ring-buffer';
 
 /**
  * Main loop for the PortAllocator daemon.
@@ -18,14 +25,29 @@ import { BaseServer, type Handlers } from 'util/protocol';
 export async function main(ns: NS) {
     await parseFlags(ns, []);
 
-    ns.disableLog('sleep');
+    ns.disableLog('ALL');
+    ns.clearLog();
+    ns.ui.openTail();
+    ns.ui.setTailTitle('Port Allocator');
 
+    const { ns: logNS, buffer } = installLogger(ns, { bufferCap: 100 });
+
+    await startAllocator(logNS, buffer);
+}
+
+async function startAllocator(ns: NS, buffer: RingBuffer<string>) {
     const allocator = new PortAllocator(ns);
 
     const memClient = new MemoryClient(ns);
 
     const self = ns.self();
     memClient.registerAllocation(self.server, self.ramUsage, 1);
+
+    ns.printRaw(
+        <LogRoot ns={ns} buffer={buffer}>
+            <PortDisplay ns={ns} allocator={allocator} />
+        </LogRoot>,
+    );
 
     const server = new Server(ns, allocator);
     await server.readLoop();
@@ -77,4 +99,43 @@ class PortAllocator {
         this.allocated.delete(id);
         this.ns.clearPort(id);
     }
+}
+
+interface PortDisplayProps {
+    ns: NS;
+    allocator: PortAllocator;
+}
+
+function PortDisplay({ ns, allocator }: PortDisplayProps) {
+    const theme = useTheme(ns);
+    const nextPort = usePoll(ns, 200, () => allocator.nextPort);
+    const ports = useNsUpdate(ns, 200, (ns) =>
+        Array.from(allocator.allocated).map((p) => ({
+            id: p,
+            empty: ns.getPortHandle(p).empty(),
+            full: ns.getPortHandle(p).full(),
+        })),
+    );
+    return (
+        <div>
+            <div>Next port: {nextPort}</div>
+            {ports.map((p) => (
+                <div key={p.id}>
+                    {p.id}{' '}
+                    <span style={{ color: color(theme, p) }}>{icon(p)}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function color(
+    theme: UserInterfaceTheme,
+    p: { empty: boolean; full: boolean },
+): string {
+    return p.empty ? theme.success : !p.full ? theme.money : theme.error;
+}
+
+function icon(p: { empty: boolean; full: boolean }): string {
+    return p.empty ? '✓' : !p.full ? '⚬' : '✗';
 }
