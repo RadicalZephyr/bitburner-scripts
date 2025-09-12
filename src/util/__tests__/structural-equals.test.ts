0919bf12379376fe8493ace4b524e1ff281b9d90
Zefira Shannon <zefira@hey.com>
2025-09-12 07:30:52 -0700

    [util] Compare objects key-wise

--- src/util/__tests__/structural-equals.test.ts
+++ src/util/__tests__/structural-equals.test.ts
@@ -19,6 +19,7 @@ describe('isStructuralEqual', () => {
                 [0n, 0],
                 [0n, new String('0')],
                 [7, '7'],
+                [{}, null],
             ])('%s !== %s', (a, b) => {
                 expect(isStructuralEqual(a, b)).toBeFalsy();
                 expect(isStructuralEqual(b, a)).toBeFalsy();
@@ -43,6 +44,33 @@ describe('isStructuralEqual', () => {
 
         test("numeric arrays with same elements in a different order aren't equal", () => {
             expect(isStructuralEqual([1, 2, 3], [2, 1, 3])).toBeFalsy();
+        });
+    });
+
+    describe('objects compare key-wise', () => {
+        test('empty objects are equal', () => {
+            expect(isStructuralEqual({}, {})).toBeTruthy();
+        });
+
+        test('objects with one key are equal', () => {
+            expect(isStructuralEqual({ a: 1 }, { a: 1 })).toBeTruthy();
+        });
+
+        test('objects with recursive objects all with same keys are equal', () => {
+            expect(
+                isStructuralEqual(
+                    { c: { a: 1, b: ['foo'] } },
+                    { c: { a: 1, b: ['foo'] } },
+                ),
+            ).toBeTruthy();
+        });
+
+        test('objects with same keys and different values are not equal', () => {
+            expect(isStructuralEqual({ a: 1 }, { a: 'foo' })).toBeFalsy();
+        });
+
+        test('objects with different keys are not equal', () => {
+            expect(isStructuralEqual({ a: 1 }, { b: 1 })).toBeFalsy();
         });
     });
 });
--- src/util/structural-equals.ts
+++ src/util/structural-equals.ts
@@ -1,3 +1,5 @@
+import { isObjectUnknown } from 'util/validate';
+
 /**
  * Compare two values structurally.
  */
@@ -7,6 +9,9 @@ export function isStructuralEqual(a: unknown, b: unknown): boolean {
     if (typeof a === 'object') {
         if (Array.isArray(a) && Array.isArray(b))
             return isArrayStructuralEquals(a, b);
+
+        if (isObjectUnknown(a) && isObjectUnknown(b))
+            return isObjectStructuralEqual(a, b);
     }
 
     return a === b;
@@ -20,6 +25,20 @@ export function isArrayStructuralEquals(
 
     for (let i = 0; i < a.length && i < b.length; i++) {
         if (!isStructuralEqual(a[i], b[i])) return false;
+    }
+
+    return true;
+}
+
+export function isObjectStructuralEqual(a: object, b: object): boolean {
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+    if (aKeys.length !== bKeys.length) return false;
+
+    for (const k of aKeys) {
+        if (!(k in b)) return false;
+
+        if (!isStructuralEqual(a[k], b[k])) return false;
     }
 
     return true;
