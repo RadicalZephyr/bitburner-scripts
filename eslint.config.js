fac56bc4bd1c7971d2783f077d0fb3589a4feb5b
Zefira Shannon <zefira@hey.com>
2025-09-15 11:41:11 -0700

    [eslint.config.js] Add eslint plugin for checking react hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
 
-export default tseslint.config(
+export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommended,
+    reactHooks.configs['recommended-latest'],
 );
