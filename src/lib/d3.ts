556a8c8fa7f316c7056d4c65e664653dcb2ba51d
Zefira Shannon <zefira@hey.com>
2025-09-15 09:23:45 -0700

    [lib] Add SRI hashes to validate d3 and plot scripts

--- src/lib/d3.ts
+++ src/lib/d3.ts
@@ -5,4 +5,9 @@ import { importFromGlobal } from 'util/script-import';
 export const d3 = await importFromGlobal<typeof import('d3')>(
     'https://cdn.jsdelivr.net/npm/d3@7.9.0/dist/d3.min.js',
     'd3',
+    {
+        integrity:
+            'sha384-CjloA8y00+1SDAUkjs099PVfnY2KmDC2BZnws9kh8D/lX1s46w6EPhpXdqMfjK6i',
+        crossOrigin: 'anonymous',
+    },
 );
--- src/lib/plot.ts
+++ src/lib/plot.ts
@@ -7,4 +7,9 @@ import { importFromGlobal } from 'util/script-import';
 export const Plot = await importFromGlobal<typeof import('@observablehq/plot')>(
     'https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6.17/dist/plot.umd.min.js',
     'Plot',
+    {
+        integrity:
+            'sha384-JUpn2GgRr0gxU0xOBd8D8P634jhRCwobtG8G2MMEkX1RnGJ7/FJNnuukpfT+H2w1',
+        crossOrigin: 'anonymous',
+    },
 );
