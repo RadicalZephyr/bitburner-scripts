71713f25c50d753830531fcf6e056b5496833981
Zefira Shannon <zefira@hey.com>
2025-09-16 08:26:19 -0700

    [services] Sketch out a Sodium based API for the discover service

--- src/services/client/discover.ts
+++ src/services/client/discover.ts
@@ -10,6 +10,33 @@ import {
     Validator,
 } from 'util/validate';
 
+import { Cell, CellSink, Stream } from 'lib/sodium';
+
+type Hostname = string;
+
+class HostSource {
+    readonly #newHostsSource: CellSink<Stream<Hostname>> = new CellSink(
+        new Stream(),
+    );
+    readonly newHosts: Stream<Hostname> = Cell.switchS(this.#newHostsSource);
+
+    readonly hosts: Cell<Set<Hostname>> = this.newHosts.accum<Set<Hostname>>(
+        new Set() satisfies Set<Hostname>,
+        (newHost, hosts) => {
+            hosts.add(newHost);
+            return hosts;
+        },
+    );
+
+    set newHostsSource(source: Stream<Hostname>) {
+        this.#newHostsSource.send(source);
+    }
+}
+
+export const WorkerSource = new HostSource();
+
+export const TargetSource = new HostSource();
+
 export const DISCOVERY_PORT = 1;
 export const DISCOVERY_RESPONSE_PORT = 2;
 
