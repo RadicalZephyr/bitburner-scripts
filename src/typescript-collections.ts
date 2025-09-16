dabedda8b46d9b1a9cd0cb71a2abef694a72c893
Zefira Shannon <zefira@hey.com>
2025-09-16 05:42:34 -0700

    [typescript-collections] Add file extensions to typescript-collections module paths

--- src/typescript-collections.ts
+++ src/typescript-collections.ts
@@ -3,22 +3,22 @@
 // Licensed under MIT open source license http://opensource.org/licenses/MIT
 //
 // Orginal javascript code was by Mauricio Santos
-import * as _arrays from 'typescript-collections/arrays';
+import * as _arrays from 'typescript-collections/arrays.js';
 export const arrays = _arrays;
-export { default as Bag } from 'typescript-collections/Bag';
-export { default as BSTree } from 'typescript-collections/BSTree';
-export { default as BSTreeKV } from 'typescript-collections/BSTreeKV';
-export { default as Dictionary } from 'typescript-collections/Dictionary';
-export { default as Heap } from 'typescript-collections/Heap';
-export { default as LinkedDictionary } from 'typescript-collections/LinkedDictionary';
-export { default as LinkedList } from 'typescript-collections/LinkedList';
-export { default as MultiDictionary } from 'typescript-collections/MultiDictionary';
-export { default as FactoryDictionary } from 'typescript-collections/FactoryDictionary';
-export { default as DefaultDictionary } from 'typescript-collections/FactoryDictionary';
-export { default as Queue } from 'typescript-collections/Queue';
-export { default as PriorityQueue } from 'typescript-collections/PriorityQueue';
-export { default as Set } from 'typescript-collections/Set';
-export { default as Stack } from 'typescript-collections/Stack';
-export { default as MultiRootTree } from 'typescript-collections/MultiRootTree';
-import * as _util from 'typescript-collections/util';
+export { default as Bag } from 'typescript-collections/Bag.js';
+export { default as BSTree } from 'typescript-collections/BSTree.js';
+export { default as BSTreeKV } from 'typescript-collections/BSTreeKV.js';
+export { default as Dictionary } from 'typescript-collections/Dictionary.js';
+export { default as Heap } from 'typescript-collections/Heap.js';
+export { default as LinkedDictionary } from 'typescript-collections/LinkedDictionary.js';
+export { default as LinkedList } from 'typescript-collections/LinkedList.js';
+export { default as MultiDictionary } from 'typescript-collections/MultiDictionary.js';
+export { default as FactoryDictionary } from 'typescript-collections/FactoryDictionary.js';
+export { default as DefaultDictionary } from 'typescript-collections/FactoryDictionary.js';
+export { default as Queue } from 'typescript-collections/Queue.js';
+export { default as PriorityQueue } from 'typescript-collections/PriorityQueue.js';
+export { default as Set } from 'typescript-collections/Set.js';
+export { default as Stack } from 'typescript-collections/Stack.js';
+export { default as MultiRootTree } from 'typescript-collections/MultiRootTree.js';
+import * as _util from 'typescript-collections/util.js';
 export const util = _util;
