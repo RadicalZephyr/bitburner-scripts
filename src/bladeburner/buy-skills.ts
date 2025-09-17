b3bb9eff9203bbc195cfe0ce384b64bb970df7d3
Zefira Shannon <zefira@hey.com>
2025-09-16 21:17:04 -0700

    [bladeburner] Make buy-skills script more flexible
    Follow an option pattern similar to graft/queue-augs with presets
    groups of skills and also specifying any number of skills by name. If
    no skills or presets are specified, fall back to purchasing all
    skills.

--- src/bladeburner/buy-skills.ts
+++ src/bladeburner/buy-skills.ts
@@ -3,13 +3,54 @@ import { FlagsSchema, parseFlags } from 'util/flags';
 
 import { CONFIG } from 'bladeburner/config';
 
+const SKILLS = [
+    "Blade's Intuition",
+    'Cloak',
+    'Short-Circuit',
+    'Digital Observer',
+    'Tracer',
+    'Overclock',
+    'Reaper',
+    'Evasive System',
+    'Datamancer',
+    "Cyber's Edge",
+    'Hands of Midas',
+    'Hyperdrive',
+] as const satisfies readonly `${BladeburnerSkillName}`[];
+
+const PRESETS = {
+    'black-ops': [
+        "Blade's Intuition",
+        'Digital Observer',
+        'Overclock',
+        'Reaper',
+        'Evasive System',
+        'Hyperdrive',
+    ],
+} as const satisfies Record<string, readonly `${BladeburnerSkillName}`[]>;
+
 const FLAGS = [
-    ['black-ops', false],
+    ['skill', []],
+    ['preset', [] as string[]],
     ['help', false],
 ] as const satisfies FlagsSchema;
 
-export function autocomplete(data: AutocompleteData): string[] {
+export function autocomplete(data: AutocompleteData, args: string[]): string[] {
     data.flags(FLAGS);
+
+    const skillFlag = '--skill';
+    const presetFlag = '--preset';
+
+    const last = args.at(-1);
+    if (last === skillFlag) return SKILLS.map((s) => `"${s}"`);
+    if (last === presetFlag) return Object.keys(PRESETS);
+
+    const secondLast = args.at(-2);
+    if (secondLast === skillFlag)
+        return SKILLS.map((s) => `"${s}"`).filter((s) => s.startsWith(last));
+    if (secondLast === presetFlag)
+        return Object.keys(PRESETS).filter((p) => p.startsWith(last));
+
     return [];
 }
 
@@ -22,11 +63,15 @@ USAGE: run ${ns.getScriptName()}
 
 Continuously buy the cheapest Bladeburner skill available.
 
+If no skills or presets are specified then all skills are purchased.
+
 Example:
-  > run ${ns.getScriptName()}
+  > run ${ns.getScriptName()} --skill Hyperdrive
+  > run ${ns.getScriptName()} --preset black-ops
 
 OPTIONS
-  --black-ops  Prioritize skills for Black Ops success
+  --skill      Add a skill to buy (can be specified multiple times)
+  --presets    Add a predefined group of skills to buy (can be specified multiple times)
   --help       Show this help message
 
 CONFIGURATION
@@ -38,15 +83,56 @@ CONFIGURATION
     ns.disableLog('asleep');
     ns.disableLog('sleep');
 
-    await buySkills(ns, flags['black-ops']);
+    const skills = buildSkills(ns, flags.skill, flags.preset);
+    await buySkills(ns, skills);
 }
 
-async function buySkills(ns: NS, blackOps: boolean) {
-    while (true) {
-        const skillNames = blackOps
-            ? blackOpsSkills()
-            : ns.bladeburner.getSkillNames();
+function buildSkills(
+    ns: NS,
+    skills: string[],
+    presets: string[],
+): `${BladeburnerSkillName}`[] {
+    if (skills.length === 0 && presets.length === 0) return [...SKILLS];
 
+    const allSkills = new Set(ns.bladeburner.getSkillNames() as string[]);
+    const isSkill = (s: string): s is `${BladeburnerSkillName}` =>
+        allSkills.has(s);
+
+    const skillsToBuy = new Set<`${BladeburnerSkillName}`>();
+    for (const s of skills) {
+        const skill = s.trim();
+        if (isSkill(skill)) {
+            skillsToBuy.add(skill);
+        } else {
+            ns.tprint(`WARN: Unknown Bladeburner skill: '${skill}'`);
+        }
+    }
+
+    for (const p of presets) {
+        const preset = p.trim().toLocaleLowerCase();
+        if (!(preset in PRESETS)) {
+            ns.tprint(`WARN: unknown preset: '${preset}'`);
+            continue;
+        }
+
+        const presetSkills = PRESETS[preset].filter(isSkill);
+        for (const s of presetSkills) {
+            const skill = s.trim();
+            if (isSkill(skill)) {
+                skillsToBuy.add(skill);
+            } else {
+                ns.tprint(
+                    `WARN: Unknown Bladeburner skill '${skill}' in preset '${preset}'`,
+                );
+            }
+        }
+    }
+
+    return Array.from(skillsToBuy);
+}
+
+async function buySkills(ns: NS, skillNames: `${BladeburnerSkillName}`[]) {
+    while (true) {
         const skills = skillNames
             .map((s) => new Skill(ns, s))
             .sort((a, b) => a.cost - b.cost);
@@ -86,15 +172,4 @@ async function untilPoints(ns: NS, points: number) {
     while (ns.bladeburner.getSkillPoints() < points) {
         await ns.asleep(1_000);
     }
-}
-
-function blackOpsSkills(): SkillName[] {
-    return [
-        "Blade's Intuition",
-        'Digital Observer',
-        'Overclock',
-        'Reaper',
-        'Evasive System',
-        'Hyperdrive',
-    ];
 }
