e8473c8c1e298004a868bad736c289ce5dfd2fe6
Zefira Shannon <zefira@hey.com>
2025-09-18 08:46:25 -0700

    [bladeburner] Filter out skills that we hit the level cap for

--- src/bladeburner/buy-skills.ts
+++ src/bladeburner/buy-skills.ts
@@ -138,7 +138,7 @@ async function buySkills(ns: NS, skillNames: `${BladeburnerSkillName}`[]) {
     while (true) {
         const skills = skillNames
             .map((s) => new Skill(ns, s))
-            .filter((s) => Number.isFinite(s.cost))
+            .filter((s) => Number.isFinite(s.cost) && s.levelsToBuy > 0)
             .sort((a, b) => a.cost - b.cost);
 
         if (skills.length < 1) throw new Error(`empty skills list!`);
