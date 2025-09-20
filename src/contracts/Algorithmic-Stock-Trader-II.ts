77877df6146104737ea72dc35022ae2c89f786a4
Zefira Shannon <zefira@hey.com>
2025-09-20 12:57:30 -0700

    [contracts] Add contract data runtime validation for Algorithmic Stock Trader II

--- src/contracts/Algorithmic-Stock-Trader-II.ts
+++ src/contracts/Algorithmic-Stock-Trader-II.ts
@@ -16,6 +16,7 @@
 
 import type { NS } from '@ns';
 import { parseFlags } from 'util/flags';
+import { isArrayOf, isNumber, Validator } from 'util/validate';
 
 export async function main(ns: NS) {
     await parseFlags(ns, []);
@@ -37,11 +38,19 @@ export async function main(ns: NS) {
         );
         return;
     }
-    const contractData = JSON.parse(contractDataJSON);
+    const contractData = JSON.parse(contractDataJSON) as unknown;
+
+    if (!isContractData(contractData)) {
+        ns.writePort(contractPortNum, JSON.stringify(null));
+        return;
+    }
+
     ns.tprintf('contract data: %s', JSON.stringify(contractData));
     const answer = solve(contractData);
     ns.writePort(contractPortNum, JSON.stringify(answer));
 }
+
+const isContractData: Validator<number[]> = isArrayOf(isNumber);
 
 export function solve(data: number[]): number {
     let profit = 0;
