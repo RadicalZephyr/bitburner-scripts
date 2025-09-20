06cd9689edeaa073b12fc45ac7a77d32265f33f7
Zefira Shannon <zefira@hey.com>
2025-09-20 13:36:50 -0700

    [contracts] Add contract data runtime validation for Compression I RLE Compression

--- src/contracts/Compression-I-RLE-Compression.ts
+++ src/contracts/Compression-I-RLE-Compression.ts
@@ -19,6 +19,7 @@ Examples:
 
 import type { NS } from '@ns';
 import { parseFlags } from 'util/flags';
+import { isString } from 'util/validate';
 
 export async function main(ns: NS) {
     await parseFlags(ns, []);
@@ -40,7 +41,14 @@ export async function main(ns: NS) {
         );
         return;
     }
-    const contractData = JSON.parse(contractDataJSON);
+
+    const contractData = JSON.parse(contractDataJSON) as unknown;
+
+    if (!isString(contractData)) {
+        ns.writePort(contractPortNum, JSON.stringify(null));
+        return;
+    }
+
     ns.tprintf('contract data: %s', JSON.stringify(contractData));
     const answer = solve(contractData);
     ns.writePort(contractPortNum, answer);
