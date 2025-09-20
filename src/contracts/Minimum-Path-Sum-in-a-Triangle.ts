6827275e9405bd50675cc498f6e4b9bc2bbf5e7d
Zefira Shannon <zefira@hey.com>
2025-09-20 13:50:29 -0700

    [contracts] Add contract data runtime validation for Minimum Path Sum in a Triangle

--- src/contracts/Minimum-Path-Sum-in-a-Triangle.ts
+++ src/contracts/Minimum-Path-Sum-in-a-Triangle.ts
@@ -29,6 +29,7 @@ The minimum path sum is 11 (2 -> 3 -> 5 -> 1).
 
 import type { NS } from '@ns';
 import { parseFlags } from 'util/flags';
+import { isArrayOf, isNumber, Validator } from 'util/validate';
 
 export async function main(ns: NS) {
     await parseFlags(ns, []);
@@ -50,11 +51,20 @@ export async function main(ns: NS) {
         );
         return;
     }
-    const contractData = JSON.parse(contractDataJSON);
+
+    const contractData = JSON.parse(contractDataJSON) as unknown;
+
+    if (!isContractData(contractData)) {
+        ns.writePort(contractPortNum, JSON.stringify(null));
+        return;
+    }
+
     ns.tprintf('contract data: %s', JSON.stringify(contractData));
     const answer = solve(contractData);
     ns.writePort(contractPortNum, JSON.stringify(answer));
 }
+
+const isContractData: Validator<number[][]> = isArrayOf(isArrayOf(isNumber));
 
 export function solve(data: number[][]): number {
     const dp = data[data.length - 1].slice();
