1e6d7229ca77bea7199c804aebc0db1c66a2d336
Zefira Shannon <zefira@hey.com>
2025-09-20 13:57:40 -0700

    [contracts] Add contract data runtime validation for Shortest Path in a Grid

--- src/contracts/Shortest-Path-in-a-Grid.ts
+++ src/contracts/Shortest-Path-in-a-Grid.ts
@@ -38,6 +38,7 @@ Answer: ''
 
 import type { NS } from '@ns';
 import { parseFlags } from 'util/flags';
+import { isArrayOf, isNumber, Validator } from 'util/validate';
 
 export async function main(ns: NS) {
     await parseFlags(ns, []);
@@ -59,11 +60,20 @@ export async function main(ns: NS) {
         );
         return;
     }
-    const contractData = JSON.parse(contractDataJSON);
+
+    const contractData = JSON.parse(contractDataJSON) as unknown;
+
+    if (!isContractData(contractData)) {
+        ns.writePort(contractPortNum, JSON.stringify(null));
+        return;
+    }
+
     ns.tprintf('contract data: %s', JSON.stringify(contractData));
     const answer = solve(contractData);
     ns.writePort(contractPortNum, answer);
 }
+
+const isContractData: Validator<number[][]> = isArrayOf(isArrayOf(isNumber));
 
 export function solve(data: number[][]): string {
     // Step 1: Determine grid dimensions and the start and goal cells.
