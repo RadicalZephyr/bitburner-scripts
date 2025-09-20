acc31df17858d207b56ffe8088aeb305bcd2a957
Zefira Shannon <zefira@hey.com>
2025-09-20 14:23:21 -0700

    [corp] Remove unneeded async on hire main function

--- src/corp/init/hire.ts
+++ src/corp/init/hire.ts
@@ -2,12 +2,12 @@ import type { NS } from '@ns';
 
 import { AGRI_DIVISION, CITIES } from 'corp/constants';
 
-export async function main(ns: NS) {
+export function main(ns: NS) {
     const corp = ns.corporation;
 
     if (!corp.hasCorporation()) {
         ns.tprint('you must start a corporation first!');
-        return;
+        return Promise.resolve();
     }
 
     const agriDiv = corp.getDivision(AGRI_DIVISION);
@@ -19,4 +19,6 @@ export async function main(ns: NS) {
             corp.hireEmployee(AGRI_DIVISION, city, 'Research & Development');
         ns.tprint(`hired 4 employees for R&D at ${AGRI_DIVISION} in ${city}`);
     }
+
+    return Promise.resolve();
 }
