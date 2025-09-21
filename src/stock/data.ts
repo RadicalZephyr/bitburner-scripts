68c3fca28b2fade0ba985e7047d042ff2f15c4a4
Zefira Shannon <zefira@hey.com>
2025-09-21 05:05:05 -0700

    [stock] Add helper function for robustly reading stored stock data

--- src/stock/data.ts
+++ src/stock/data.ts
@@ -1,4 +1,8 @@
+import { NS } from '@ns';
+
 import { isArrayOf, isNumber, isObjectLike, Validator } from 'util/validate';
+
+import { CONFIG } from 'stock/config';
 
 export interface TickData {
     ts: number;
@@ -17,3 +21,21 @@ export const isTickData: Validator<TickData> = isObjectLike({
 });
 
 export const isTickDataArray: Validator<TickData[]> = isArrayOf(isTickData);
+
+/**
+ * Read tick data for symbol from the canonical stored stock data
+ */
+export function readStoredTickData(ns: NS, sym: string): TickData[] {
+    const dataPath = CONFIG.dataPath;
+    const path = `${dataPath}${sym}.json`;
+    if (!ns.fileExists(path)) return [];
+
+    const tickData = JSON.parse(ns.read(path)) as unknown;
+    if (!isTickDataArray(tickData)) {
+        ns.print(
+            `WARN: stored tick data for symbol ${sym} format is unrecognized`,
+        );
+        return [];
+    }
+    return tickData;
+}
