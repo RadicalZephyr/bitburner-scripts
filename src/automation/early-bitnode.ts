a45c1008b7688663391b671745ce59e9c8f68a59
Zefira Shannon <zefira@hey.com>
2025-09-23 12:14:55 -0700

    [automation] Stricter types in early-bitnode

--- src/automation/early-bitnode.ts
+++ src/automation/early-bitnode.ts
@@ -125,10 +125,10 @@ async function sowAndHackNoodles(ns: NS) {
         },
         noods,
     );
-    if (sowResult != null && sowResult.pids.length > 0)
+    if (!sowResult || sowResult.pids.length > 0)
         throw new Error('failed to launch sow against n00dles');
 
-    await waitForExit(ns, sowResult.pids[0]!);
+    await waitForExit(ns, sowResult.pids[0]);
 
     const harvestResult = await client.launch(
         '/batch/harvest.js',
