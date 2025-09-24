07eb73b1a397336bfb2797259ec479f67b8a8393
Zefira Shannon <zefira@hey.com>
2025-09-23 18:37:40 -0700

    [contracts] Improve type inference for Find-All-Valid-Math-Expressions tests

--- src/contracts/__tests__/Find-All-Valid-Math-Expressions.test.ts
+++ src/contracts/__tests__/Find-All-Valid-Math-Expressions.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, test } from '@jest/globals';
 
 import { solve as solveExpr } from '../Find-All-Valid-Math-Expressions';
 
+type TestCase = [Parameters<typeof solveExpr>[0], ReturnType<typeof solveExpr>];
+
 describe('Math Expressions', () => {
     test.each([
         [
@@ -13,7 +15,10 @@ describe('Math Expressions', () => {
             ['1*0+5', '10-5'],
         ],
         [['29787', -8], ['2-97+87']],
-    ])('%s', (data: [string, number], expected: string[]) => {
-        expect(new Set(solveExpr(data))).toEqual(new Set(expected));
-    });
+    ] satisfies TestCase[])(
+        '%s',
+        (data: [string, number], expected: string[]) => {
+            expect(new Set(solveExpr(data))).toEqual(new Set(expected));
+        },
+    );
 });
