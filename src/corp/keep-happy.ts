58ee818a35450b168a8a4673ced2815861ac0d1d
Zefira Shannon <zefira@hey.com>
2025-09-25 18:30:06 -0700

    [corp] Calculate how much to spend on a party

--- src/corp/keep-happy.ts
+++ src/corp/keep-happy.ts
@@ -79,9 +79,88 @@ async function manageOfficeHappiness(
 
         const moralePct = office.avgMorale / office.maxMorale;
         if (moralePct < CONFIG.lowMoraleThreshold) {
-            // TODO: Choose how much money to spend on party based on needed morale increase
-            const budget = 1e6;
+            const budget = budgetForParty(office.avgMorale, office.maxMorale);
             ns.corporation.throwParty(division, city, budget);
         }
     }
+}
+
+/**
+ * Calculate the budget to use to get the percent increase we need.
+ *
+ * @remarks
+ *
+ * Game Code References:
+ *
+ * - src/Corporation/Actions.ts#410 - `throwParty` function
+ * - src/Corporation/OfficeSpace.ts#101 - `process` function
+ *
+ * Party morale increase is a multiplier to the current average morale
+ * calculated like so:
+ *
+ * `1 + (costPerEmployee / 10e6)`
+ *
+ * A constant amount is added before the multiplier as well, calculated like so:
+ *
+ * `(partyMult - 1) * 10`
+ *
+ * So the new morale is calculate like so (simplified):
+ *
+ * ```ts
+ * const partyMult = 1 + costPerEmployee / 10e6;
+ * const increase = (partyMult - 1) * 10;
+ * avgMorale = (avgMorale + increase) * partyMult;
+ * ```
+ *
+ * Let's rewrite this as mathematical equations using the following symbols:
+ *
+ * `B`: per-employee party budget (cost per employee)
+ * `C`: current average morale
+ * `T`: target average morale (i.e. max morale)
+ *
+ * And some intermediate values to match how the game calculates things:
+ *
+ * `M`: morale multiplier from throwing a party (`partyMult` in the code)
+ * `F`: flat morale increase from throwing a party (`increase` in the code)
+ *
+ * `M = 1 + B/10^7`
+ * `F = (M - 1) * 10`
+ * `T = (C + F) * M`
+ *
+ * Let's simplify a little bit by substituting `M` in the equation for `F`.
+ * `F = (1 + B/10^7 - 1) * 10`
+ * `F = (B/10^7) * 10`
+ * `F = B/10^6`
+ *
+ * Let's introduce a new value to simplify our budget value.
+ *
+ * `x = B / 10^6`
+ *
+ * Now we have:
+ *
+ * `M = 1 + x/10`
+ * `F = x`
+ * `T = (C + F) * M`
+ *
+ * Substituting into `T`:
+ *
+ * `T = (C + x) * (1 + x/10)`
+ *
+ * Expanding and simplifying we get:
+ *
+ * `T = (1/10)x^2 + (1 + C/10)x + C`
+ * `0 = (1/10)x^2 + (1 + C/10)x + C - T`
+ *
+ * Solving for `x` with the quadratic equation we get:
+ * https://www.wolframalpha.com/input?i=solve+%281%2F10%29x%5E2+%2B+%281+%2B+%28c+%2F+10%29%29x+%2B+%28c+-+t%29+%3D+0
+ *
+ * `x = 1/2 * (sqrt(C^2 - 20C + 40T + 100) - C - 10)`
+ * `B = (10^6)/2 * (sqrt(C^2 - 20C + 40T + 100) - C - 10)`
+ * `B = 5 * 10^5 * (sqrt(C^2 - 20C + 40T + 100) - C - 10)`
+ */
+function budgetForParty(avgMorale: number, maxMorale: number): number {
+    const C = avgMorale;
+    const T = maxMorale;
+
+    return 5e5 * (Math.sqrt(C ** 2 - 20 * C + 40 * T + 100) - C - 10);
 }
